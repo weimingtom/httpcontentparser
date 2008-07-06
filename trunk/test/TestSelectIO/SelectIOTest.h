001064f08e12440020e47e5e10cd9d5ac5d32ebf
ynkhpp <ynkhpp@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-07-06 08:22:54 +0000

    Add a Test Project for static library "selectio"

---
+++ trunk/test/TestSelectIO/SelectIOTest.h
@@ -0,0 +1,23 @@
+#ifndef _TEST_SELECTIO_SELECTIO_TEST_H__
+#define _TEST_SELECTIO_SELECTIO_TEST_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+
+
+class SelectIOTest : public CPPUNIT_NS::TestFixture {
+public:
+	SelectIOTest(void);
+	~SelectIOTest(void);
+
+public: 
+	CPPUNIT_TEST_SUITE(SelectIOTest);
+	CPPUNIT_TEST(testPreSelect);
+	CPPUNIT_TEST(testPostSelect);
+	CPPUNIT_TEST_SUITE_END();
+
+private:
+	void testPreSelect();
+	void testPostSelect();
+};
+
+#endif  // _TEST_SELECTIO_SELECTIO_TEST_H__
