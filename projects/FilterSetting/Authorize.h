f4a4803201741fcade6d13e46bbc5d897b936c1d
ynkhpp <ynkhpp@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-08-20 14:59:08 +0000

    1. add a new interface in COM. which will validate password.

---
+++ projects/FilterSetting/Authorize.h
@@ -0,0 +1,49 @@
+// Authorize.h : CAuthorize µÄÉùÃ÷
+
+#pragma once
+#include "resource.h"       // Ö÷·ûºÅ
+
+#include "FilterSetting.h"
+
+
+// CAuthorize
+
+class ATL_NO_VTABLE CAuthorize : 
+	public CComObjectRootEx<CComSingleThreadModel>,
+	public CComCoClass<CAuthorize, &CLSID_Authorize>,
+	public ISupportErrorInfo,
+	public IDispatchImpl<IAuthorize, &IID_IAuthorize, &LIBID_FilterSettingLib, /*wMajor =*/ 1, /*wMinor =*/ 0>
+{
+public:
+	CAuthorize()
+	{
+	}
+
+DECLARE_REGISTRY_RESOURCEID(IDR_AUTHORIZE)
+
+
+BEGIN_COM_MAP(CAuthorize)
+	COM_INTERFACE_ENTRY(IAuthorize)
+	COM_INTERFACE_ENTRY(IDispatch)
+	COM_INTERFACE_ENTRY(ISupportErrorInfo)
+END_COM_MAP()
+
+// ISupportsErrorInfo
+	STDMETHOD(InterfaceSupportsErrorInfo)(REFIID riid);
+
+	DECLARE_PROTECT_FINAL_CONSTRUCT()
+
+	HRESULT FinalConstruct()
+	{
+		return S_OK;
+	}
+	
+	void FinalRelease() 
+	{
+	}
+
+public:
+
+};
+
+OBJECT_ENTRY_AUTO(__uuidof(Authorize), CAuthorize)
