574a585290588189d75bad239ec23122a11f515f
ynkhpp <ynkhpp@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-08-25 13:35:26 +0000

    add some test

---
+++ trunk/test/TestFilterSetting/IAuthorizeTest.cpp
@@ -0,0 +1,53 @@
+#include "StdAfx.h"
+#include ".\iauthorizetest.h"
+#include <com\FilterSetting_i.c>
+#include <com\FilterSetting.h>
+#include <passwordtype.h>
+#include <comdef.h>
+
+IAuthorizeTest::IAuthorizeTest(void) {
+}
+
+IAuthorizeTest::~IAuthorizeTest(void) {
+}
+
+
+void IAuthorizeTest::testCheckPassword() {
+	try {
+		CoInitialize(NULL);
+		_VARIANT_BOOL passed;
+
+		// create Instance
+		IAuthorize *authorize = NULL;
+		HRESULT hr = CoCreateInstance(CLSID_Authorize, NULL, CLSCTX_LOCAL_SERVER, IID_IAuthorize, (LPVOID*)&authorize);
+		if (FAILED(hr)) {
+			CPPUNIT_ASSERT(false);
+			return ;
+		}
+		
+		VARIANT_BOOL corrected;
+		_bstr_t last_password = "123";
+		_bstr_t wrong_password = "8382";
+		_bstr_t new_password = "234";
+		authorize->checkPassword(last_password, &corrected);
+		CPPUNIT_ASSERT(true == corrected);
+
+		authorize->checkPassword(wrong_password, &corrected);
+		CPPUNIT_ASSERT(false == corrected);
+
+		authorize->checkPassword(new_password, &corrected);
+		CPPUNIT_ASSERT(false == corrected);
+
+
+		authorize->changePassword(new_password, wrong_password, &corrected);
+		CPPUNIT_ASSERT(false == corrected);
+
+		authorize->changePassword(new_password, last_password, &corrected);
+		CPPUNIT_ASSERT(true == corrected);
+		
+		authorize->Release();
+		CoUninitialize();
+	} catch (_com_error &) {
+		return;
+	}
+}
