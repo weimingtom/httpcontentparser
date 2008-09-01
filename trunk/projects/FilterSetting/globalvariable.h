410a731361889a7a8e51e69c81b112a858f955c1
ynkhpp <ynkhpp@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-01 15:41:50 +0000

    add a new interface for "web history record"

--- trunk/projects/FilterSetting/globalvariable.h
+++ trunk/projects/FilterSetting/globalvariable.h
@@ -5,11 +5,13 @@
 #include <authorize.h>
 #include <screensaver.h>
 #include <eyecaresetting.h>
+#include <webhistoryrecordersetting.h>
 
 extern GlobalSetting global_setting_;
 extern Authorize g_authorize;
 extern ScreenSaver g_screenSaver;
 extern HINSTANCE g_hInstance;
 extern EyecareSetting g_Eyecare_setting_;
+extern WebHistoryRecorderSetting g_webhistoryRecordSetting_;
 
 #endif  // _FILTER_SETTING_GLOBALVARIABLE_H__
