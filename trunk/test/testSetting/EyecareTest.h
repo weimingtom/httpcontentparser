1a51104d4f3db0c19145189c0ccb71b01915632f
ynkhpp <ynkhpp@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-02 16:33:00 +0000

    add some test for class Eyecare

--- trunk/test/testSetting/EyecareTest.h
+++ trunk/test/testSetting/EyecareTest.h
@@ -7,12 +7,14 @@
 class EyecareTest : public CPPUNIT_NS::TestFixture {
 public:
 	CPPUNIT_TEST_SUITE(EyecareTest);
+	CPPUNIT_TEST(TestTimeSetting);
+	CPPUNIT_TEST(TextForceSwitch);
 	CPPUNIT_TEST(TestPassword);
 	CPPUNIT_TEST(TestSwitchState);
-	CPPUNIT_TEST(TestTimeSetting);
 	CPPUNIT_TEST_SUITE_END();
 
 private:	
+	void TextForceSwitch();
 	void TestPassword();
 	void TestSwitchState();
 	void TestTimeSetting();
