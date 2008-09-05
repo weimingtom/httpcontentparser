ab7b73fd085ef464177907a23a8bf4681f497ef5
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-05 15:44:03 +0000

    add a text for function "strnstr" in project utility

---
+++ trunk/test/TestUtility/StrUtilityTest.h
@@ -0,0 +1,20 @@
+#ifndef _TEST_UTILITY_STR_UTILITY_H__
+#define _TEST_UTILITY_STR_UTILITY_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+
+
+class StrUtilityTest  : public CPPUNIT_NS::TestFixture  {
+public:
+	StrUtilityTest(void);
+	~StrUtilityTest(void);
+
+public:
+	CPPUNIT_TEST_SUITE(StrUtilityTest);
+	CPPUNIT_TEST(Teststrnstr);
+	CPPUNIT_TEST_SUITE_END();
+private:
+	void Teststrnstr();
+};
+
+#endif  // _TEST_UTILITY_STR_UTILITY_H__
