6042c0b918a0fdd693c7320b0ee03f09e772acb2
ynkhpp <ynkhpp@gmail.com>
2008-09-05 16:00:53 +0000

    we add a new class named "HTTPRequestPacket" which is responsible for parsing the HTTP Request.

--- libproject/Utility/HTTPRequestPacket.cpp
+++ libproject/Utility/HTTPRequestPacket.cpp
@@ -1,6 +1,6 @@
 #include "StdAfx.h"
 #include ".\httprequestpacket.h"
-
+#include <utility\strutility.h>
 
 //============================================
 const char * HTTPRequestPacket::HTTP_REQUEST_HOST = "Host: ";
@@ -29,29 +29,46 @@ int HTTPRequestPacket::getUserAgent(char *buffer, const int len) {
 	return strlen(buffer);
 }
 
-int HTTPRequestPacket::parsePacket(const char * buf, const int len) {
+void HTTPRequestPacket::reset() {
+	memset(host_, 0, sizeof(host_));
+	memset(referer_, 0, sizeof(referer_));
+	memset(useagent_, 0, sizeof(useagent_));
+}
+
+int HTTPRequestPacket::parsePacket(char * buf, const int len) {
+	using namespace strutility;
+
+	//reset buffer
+	reset();
+
 	int cnt = 0;
-	const char *p = buf;
-	const char *next = strstr(p, "\r\n");
-	while (NULL != p) {
+	char *p = buf;
+	char *next = strnstr(p, "\r\n", HTTP_REQUEST_ITEM_MAX_LENGTH);
+
+
+	// 注意p-buf < len, 保证等整个处理完之后，及时退出
+	while (NULL != p && p - buf < len) {
 		if (strstr(p, HTTP_REQUEST_HOST) == p) {
-			strncpy(host_, p + strlen(HTTP_REQUEST_HOST), getWrittenCount(next-p));
+			const int tab_length = strlen(HTTP_REQUEST_HOST);
+			strncpy(host_, p + tab_length, getWrittenCount(next-p-tab_length));
 			cnt++;
 		} else if (strstr(p, HTTP_REQUEST_REFERER) == p) {
-			strncpy(referer_, p + strlen(HTTP_REQUEST_REFERER), getWrittenCount(next-p));
+			const int tab_length = strlen(HTTP_REQUEST_REFERER);
+			strncpy(referer_, p + tab_length, getWrittenCount(next-p-tab_length));
 			cnt++;
 		} else if (strstr(p, HTTP_REQUEST_OPER_GET) == p) {
 			//strncpy(host_, p, getWrittenCount(next-p));
 			cnt++;
 		} else if (strstr(p, HTTP_REQUEST_USER_AGENT) == p) {
-			strncpy(useagent_, p + strlen(HTTP_REQUEST_USER_AGENT), getWrittenCount(next-p));
+			const int tab_length = strlen(HTTP_REQUEST_USER_AGENT);
+			strncpy(useagent_, p + tab_length, getWrittenCount(next-p-tab_length));
 			cnt++;
 		} else {
 		}
 		
 		next += 2; // 移动两个位置
 		p = next;
-		next = strstr(p, "\r\n");
+		next = strnstr(p, "\r\n", HTTP_REQUEST_ITEM_MAX_LENGTH);
 	}
 
 	return cnt;
