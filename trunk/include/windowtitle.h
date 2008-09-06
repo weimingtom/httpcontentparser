c304af334666fa1ee5820642203fed21e485ddcb
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-06 12:30:00 +0000

    add the eyecare windows class name into the header file.

--- trunk/include/windowtitle.h
+++ trunk/include/windowtitle.h
@@ -5,4 +5,9 @@
 
 #define MAIN_WINDOW_TITLE (TEXT("MainUI"))
 #define MAIN_WINDOW_CLASS (TEXT("#32770"))
+
+
+// for Eyecare windows
+#define EYECARE_MAIN_WND_CLASS (TEXT("Eyecare_main_wnd_class"))
+#define EYECARE_MAIN_WND_TITLE (TEXT(""))
 #endif  // _INCLUDE_WINDOW_TITLE_H__
