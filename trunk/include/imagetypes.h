5711798eecb53c34b81b6f712efd079c1567d9ab
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-06 13:07:22 +0000

    1. add a header file defined the image type vlaue. 2. add a header file defined the SPI Register KEY Value.

---
+++ trunk/include/imagetypes.h
@@ -0,0 +1,11 @@
+#ifndef _INCLUDE_IMAGE_TYPE_H__
+#define _INCLUDE_IMAGE_TYPE_H__
+
+#define IMAGE_TYPE		  2000
+#define IMAGE_TYPE_JPEG   (IMAGE_TYPE + 1)
+#define IMAGE_TYPE_GIF	  (IMAGE_TYPE + 2)
+#define IMAGE_TYPE_PNG	  (IMAGE_TYPE + 3)
+#define IMAGE_TYPE_BMP	  (IMAGE_TYPE + 4)
+
+
+#endif  // _INCLUDE_IMAGE_TYPE_H__
