a2b2a6d0b872be8f49cfd538cbca597090461405
ynkhpp <ynkhpp@gmail.com>
2008-09-08 13:34:30 +0000

    1. modify the imagetype's number, so that we can check it well binary operation.

--- include/imagetypes.h
+++ include/imagetypes.h
@@ -1,11 +1,13 @@
 #ifndef _INCLUDE_IMAGE_TYPE_H__
 #define _INCLUDE_IMAGE_TYPE_H__
 
-#define IMAGE_TYPE		  2000
-#define IMAGE_TYPE_JPEG   (IMAGE_TYPE + 1)
-#define IMAGE_TYPE_GIF	  (IMAGE_TYPE + 2)
-#define IMAGE_TYPE_PNG	  (IMAGE_TYPE + 3)
-#define IMAGE_TYPE_BMP	  (IMAGE_TYPE + 4)
 
+#define IMAGE_TYPE_JPEG   0x00000001
+#define IMAGE_TYPE_GIF	  0x00000002
+#define IMAGE_TYPE_PNG	  0x00000004
+#define IMAGE_TYPE_BMP	  0x00000008
+
+// so we can use binary test whether this
+// type of images should be check or not.
 
 #endif  // _INCLUDE_IMAGE_TYPE_H__
