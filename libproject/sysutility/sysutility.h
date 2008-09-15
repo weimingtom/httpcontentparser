ec8f8f76d453a65df5e3e55aee39f604ee49cee5
ynkhpp <ynkhpp@gmail.com>
2008-09-15 14:59:07 +0000

    add a new function which can get the directory from the fullpath

--- libproject/sysutility/sysutility.h
+++ libproject/sysutility/sysutility.h
@@ -1,7 +1,6 @@
 #ifndef _FILTER_SETTING_SYS_UTILITY_H__
 #define _FILTER_SETTING_SYS_UTILITY_H__
 
-void RegisterServiceProcess();
 
 // 保存屏幕
 void SaveScreen(HMODULE hModule);
@@ -13,5 +12,9 @@ void StartEyecare(HMODULE hModule);
 
 // 清空所有
 void ClearHistory(HMODULE hModule);
+
+
+// 获取指定文件的目录
+char* GetFileNameDir(const TCHAR *filename, TCHAR *directory, const int len);
 
 #endif  // _FILTER_SETTING_SYS_UTILITY_H__
