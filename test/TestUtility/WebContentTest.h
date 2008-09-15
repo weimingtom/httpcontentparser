bf9ce0a06f15d43544786a718adbfffd1a1c9f13
ynkhpp <ynkhpp@gmail.com>
2008-09-15 14:58:20 +0000

    add some test for webcontenttype's utility functions

---
+++ test/TestUtility/WebContentTest.h
@@ -0,0 +1,23 @@
+#ifndef _TEST_UTILITY_WEB_CONTENTTEST_H__
+#define _TEST_UTILITY_WEB_CONTENTTEST_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class WebContentTest : public CPPUNIT_NS::TestFixture {
+public:
+	WebContentTest(void);
+	~WebContentTest(void);
+
+	CPPUNIT_TEST_SUITE(WebContentTest);
+	CPPUNIT_TEST(testImageType);
+	CPPUNIT_TEST(testTextType);
+	CPPUNIT_TEST(textOtherTypes);
+	CPPUNIT_TEST_SUITE_END();
+
+private:
+	void testImageType();
+	void testTextType();
+	void textOtherTypes();
+};
+
+#endif  // _TEST_UTILITY_WEB_CONTENTTEST_H__
