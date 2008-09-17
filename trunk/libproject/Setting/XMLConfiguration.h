ed3b2fccbb081db0f757c853c23141d7d8e39434
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-17 15:21:08 +0000

    middle version, not implemented completely

--- trunk/libproject/Setting/XMLConfiguration.h
+++ trunk/libproject/Setting/XMLConfiguration.h
@@ -30,7 +30,12 @@
 #define CONFIG_NODE_RULES			TEXT("rules")
 #define CONFIG_NODE_APPSET			TEXT("appsetting")
 
-#define CONFIG_NODE_NAME			TEXT("name")
+#define CONFIG_CONST_NAME			TEXT("name")
+#define CONFIG_CONST_TYPPE			TEXT("type")
+#define CONFIG_CONST_ENABLE			TEXT("enable")
+#define CONFIG_CONST_ENABLE_TRUE	TEXT("true")
+#define CONFIG_CONST_ENABLE_FALSE	TEXT("false")
+
 #define CONFIG_NODE_NAME_BLACKURL	TEXT("blackurl")
 #define CONFIG_NODE_NAME_WHITEURL	TEXT("whiteurl")
 #define CONFIG_NODE_NAME_ONLINETIME	TEXT("onlinetime")
@@ -38,9 +43,6 @@
 #define CONFIG_NODE_NAME_IMAGECHECK	TEXT("imagecheck")
 #define CONFIG_NODE_NAME_TEXT		TEXT("textrule")
 
-#define CONFIG_NODE_ENABLE			TEXT("enable")
-#define CONFIG_NODE_ENABLE_TRUE		TEXT("true")
-#define CONFIG_NODE_ENABLE_FALSE	TEXT("false")
 
 #define CONFIG_NODE_IMAGETYPE		TEXT("imagetype")
 #define CONFIG_NODE_IMAGETYPE_BMP	TEXT("bmp")
@@ -48,16 +50,44 @@
 #define CONFIG_NODE_IMAGETYPE_PNG	TEXT("png")
 #define CONFIG_NODE_IMAGETYPE_JPG	TEXT("jpg")
 
-#define CONFIG_NODE_SEARCH_ENGINE	TEXT("searchengine")
-#define CONFIG_NODE_BLACK_SEARCHWORD TEXT("blackwords")
+#define CONFIG_NODE_SEARCH_ENGINE		TEXT("searchengine")
+#define CONFIG_NODE_BLACK_SEARCHWORD	TEXT("blackwords")
 
-#define CONFIG_NODE_APPSET_AUTHORIZE	TEXT("authorize")
-#define CONFIG_NODE_APPSET_EYECARE		TEXT("eyecare")
-#define CONFIG_NODE_APPSET_WEBHISTORY	TEXT("webhistory")
-#define CONFIG_NODE_APPSET_SYSSETTING	TEXT("syssetting")
+// Autorize
+#define CONFIG_ITEM_APPSET_AUTHORIZE		TEXT("authorize")
+#define CONFIG_APPSET_AUTHORIZE_USER		TEXT("user")
+#define CONFIG_APPSET_AUTHORIZE_NAME		TEXT("username")
+#define CONFIG_APPSET_AUTHORIZE_PASSWORD	TEXT("password")
+#define CONFIG_APPSET_AUTHORIZE_USERTYPE_SU	TEXT("su")
+
+
+#define CONFIG_ITEM_APPSET_SYSSETTING	TEXT("syssetting")
+
+// Eyecare
+#define CONFIG_ITEM_APPSET_EYECARE		TEXT("eyecare")
+#define CONFIG_APPSET_EYECARE_TIME		TEXT("time")
+#define CONFIG_APPSET_EYECARE_TIMESPAN TEXT("timespan")
+#define CONFIG_APPSET_EYECARE_REST		TEXT("rest")
+#define CONFIG_APPSET_EYECARE_ENTER		TEXT("entertain")
+#define CONFIG_APPSET_EYECARE_STATE		TEXT("state")
+#define CONFIG_APPSET_EYECARE_TIMELEFT	TEXT("timeleft")
+
+// WebHistory
+#define CONFIG_ITEM_APPSET_WEBHISTORY			TEXT("webhistory")
+#define CONFIG_APPSET_WEBHISTORY_CONTENT		TEXT("content")
+#define CONFIG_APPSET_WEBHISTORY_ALL_IMAGE		TEXT("all_images")
+#define CONFIG_APPSET_WEBHISTORY_PORN_IMAGE		TEXT("porn_images")
+#define CONFIG_APPSET_WEBHISTORY_ALL_WEBSITE	TEXT("all_website")
+#define CONFIG_APPSET_WEBHISTORY_PORN_WEBSITE	TEXT("porn_website")
+#define CONFIG_APPSET_WEBHISTORY_ALL_WEBPAGE	TEXT("all_webpage")
+#define CONFIG_APPSET_WEBHISTORY_PORN_WEBPAGE	TEXT("porn_webpage")
 
 #include <DNSSetting.h>
 #include <SearchRule.h>
+#include <onlinehoursetting.h>
+#include <eyecaresetting.h>
+#include <authorize.h>
+#include <webhistoryrecordersetting.h>
 #include <string>
 #include <set>
 #include <map>
@@ -76,47 +106,69 @@ public:
 	DNSList black_url_set;
 	DNSList white_url_set;
 	SearchRule search_rule;
+	OnlineHourSetting online_setting;
+	EyecareSetting eye_care;
+	WebHistoryRecorderSetting web_history;
+	Authorize authorize;
 private:
 	// 读取XML
 	int readConfig();
 	int readDefaultConfig();
-	int readConfigFromFile(const char *filename);
-	char * getConfigFilePath(char *buffer, const int bufsize);
+	int readConfigFromFile(const TCHAR *filename);
+	TCHAR * getConfigFilePath(TCHAR *buffer, const int bufsize);
 
 	// 分析XML
 	int parseConfiguration(TiXmlElement * root);
 	int parseRules(TiXmlNode *rules_root);
 	int parseAppSet(TiXmlNode *appset_root);
 
+	// 分析应用程序设置
+	int getSystemSetting(TiXmlElement * ele);
+	
+	// Authorize
+	int getAuthorizeSetting(TiXmlElement *ele);
+	int addUser(const TCHAR *username, const TCHAR *password);
+
+	// Eyecare
+	int getEyecareSetting(TiXmlElement *ele);
+	int setEyecareSetting(const TCHAR *type, const TCHAR *time);
+	int enableEyecareSetting(const TCHAR *enable);
+	int setEyecareState(const TCHAR *state, const TCHAR *timeleft);
+
+	// WebHistory
+	int getWebHistoryRecorder(TiXmlElement *ele);
+	int enableWebHistoryRecord(const TCHAR *enable);
+	int setWebHistoryRecord(const TCHAR *type, const TCHAR *enable);
+
 	// 分析各个规则
 	int getWhiteURL(TiXmlElement * ele);
 	int getBlackURL(TiXmlElement * ele);
+	int addWhiteURL(const TCHAR *URL);
+	int enableWhiteURL(const TCHAR *enble);
+	int addBlackURL(const TCHAR *URL);
+	int enableBlackURL(const TCHAR *enble);
+
+	// 上网规则
 	int getOnlinetime(TiXmlElement * ele);
+	int onlineBlocktime(const TCHAR *time);
+
+	// 搜索规则
 	int getSearchRule(TiXmlElement * ele);
-	int getImageRule(TiXmlElement * ele);
+	int setSearchEngineCheck(const TCHAR *word);
+	int addBlackSearchWord(const TCHAR *word);
 	int getSearchBlackWord(TiXmlElement * ele);
 	int getSearchEngineSetting(TiXmlElement * ele);
+	int setSearchEngineCheck(const TCHAR *search_engine, const TCHAR *enable);
 
-	int addWhiteURL(const char *URL);
-	int enableWhiteURL(const char *enble);
-	int addBlackURL(const char *URL);
-	int enableBlackURL(const char *enble);
-	int onlineBlocktime(const char *time);
-	int setImageCheck(const char *imagetype, const char *enable);
-	int setTextCheck(const char *language, const char *enable);
-	int addSearchBlackWord(const char *word);
-	int setSearchEngineCheck(const char *word);
-	int addBlackSearchWord(const char *word);
-	int setSearchEngineCheck(const char *search_engine, const char *enable);
-	bool checkEnable(const char *enable) const;
-public:
-	// 修改规则的函数
+	// 处理各个规则的具体细节
+	int getImageRule(TiXmlElement * ele);
+	int setImageCheck(const TCHAR *imagetype, const TCHAR *enable);
+	int setTextCheck(const TCHAR *language, const TCHAR *enable);
+
+	
+	bool checkEnable(const TCHAR *enable) const;
 private:
-	bool changed_;	//  是否有规则改变
-private:
-	// 配置信息
-	// 注： 所有的配置信息都应该在构造函数中初始化，这样
-	// 即使无法读取到配置信息也可以读取也能够使用默认配置启动软件
+	// 保存规则的函数
 };
 
 #endif  // _SETTING_XMLCONFIGURATION_H__
