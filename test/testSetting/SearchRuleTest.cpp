8a3c8eba664e82b7ea96e3a0c860dc63cb5caec4
ynkhpp <ynkhpp@gmail.com>
2008-09-18 15:53:58 +0000

    test the new function of class SearchRule.

--- test/testSetting/SearchRuleTest.cpp
+++ test/testSetting/SearchRuleTest.cpp
@@ -35,5 +35,31 @@ void SearchRuleTest::TestSeachRuleSetting() {
 	CPPUNIT_ASSERT(true == search_rule.check(search_host1, no_word));
 }
 
-void SearchRuleTest::TestSeachRuleCheck() {
+void SearchRuleTest::TestSeachEnabled() {
+	const string search_word1 = "hello", no_word="hello2";
+	const string search_host1 = "google", search_host2 = "yahoo";
+	SearchRule search_rule;
+	search_rule.addBlackSearchWord("hello");
+	search_rule.addSearchHost(search_host1);
+	search_rule.addSearchHost(search_host2);
+	search_rule.enableCheck(search_host1, true);
+	search_rule.enableCheck(search_host2,  false);
+
+	CPPUNIT_ASSERT(false == search_rule.check(search_host1, search_word1));
+	CPPUNIT_ASSERT(true == search_rule.check(search_host2, search_word1));
+
+	search_rule.enableCheck(search_host1, false);
+	search_rule.enableCheck(search_host2, true);
+	search_rule.enable(false);
+
+	CPPUNIT_ASSERT(true == search_rule.check(search_host1, search_word1));
+	CPPUNIT_ASSERT(true == search_rule.check(search_host2, search_word1));
+	CPPUNIT_ASSERT(true == search_rule.check(search_host1, no_word));
+	CPPUNIT_ASSERT(true == search_rule.check(search_host1, no_word));
+
+	search_rule.enable(true);
+	CPPUNIT_ASSERT(true == search_rule.check(search_host1, search_word1));
+	CPPUNIT_ASSERT(false == search_rule.check(search_host2, search_word1));
+	CPPUNIT_ASSERT(true == search_rule.check(search_host1, no_word));
+	CPPUNIT_ASSERT(true == search_rule.check(search_host1, no_word));
 }
