32b2059f4e21d7692fdcd203ba6eb8a6ed31d0a3
ynkhpp <ynkhpp@gmail.com>
2008-09-18 15:53:32 +0000

    add two new function, "enable" and "isEnabled"

--- libproject/Setting/SearchRule.cpp
+++ libproject/Setting/SearchRule.cpp
@@ -2,6 +2,7 @@
 #include ".\SearchRule.h"
 
 SearchRule::SearchRule(void) {
+	enabled_ = true;
 }
 
 SearchRule::~SearchRule(void) {
@@ -19,25 +20,16 @@ int SearchRule::enableCheck(const std::string &search_host, const bool checked) 
 
 // 首先检测host_name, 如果不需要检测直接返回
 // 然后检测搜索词是否在word_set当中
-bool SearchRule::check(const std::string &host_name,
-					   const std::string &search_word) const {
-	if (false == shouldCheck(host_name)) {
+bool SearchRule::check(const std::string &host_name,  const std::string &search_word) const {
+	if (isEnabled() == false) {
+		return true; 
+	} else if (false == shouldCheck(host_name)) {
 		return true;
 	} else {
 		return checkWord(search_word);
 	}
 }
 
-// 检测word是否合法
-// 如果word不在我们的set当中，则他是合法的, 返回true
-// 否则返回false
-bool SearchRule::checkWord(const std::string &word) const {
-	if (word_set_.find(word) == word_set_.end()) {
-		return true;
-	} else {
-		return false;
-	}
-}
 
 // 首先查看host_name, 是否在我们的host_map当中, 如果不存在直接返回false
 // 否则， 根据用户设置返回
@@ -47,5 +39,18 @@ bool SearchRule::shouldCheck(const std::string &search_host) const {
 		return false;
 	} else {
 		return iter->second;
+	}
+}
+
+//=========================================================
+// private members
+// 检测word是否合法
+// 如果word不在我们的set当中，则他是合法的, 返回true
+// 否则返回false
+bool SearchRule::checkWord(const std::string &word) const {
+	if (word_set_.find(word) == word_set_.end()) {
+		return true;
+	} else {
+		return false;
 	}
 }
