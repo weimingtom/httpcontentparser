483d836ef93386ef563a13818dc9d4ebde1aab2d
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-19 11:50:50 +0000

    1. add a new convert from bool to VARIANT_BOOL 2. add assert if the value not equal false or true, VARIANT_TRUE or VARIANT_FALSE, precisely.

--- trunk/include/typeconvert.h
+++ trunk/include/typeconvert.h
@@ -1,11 +1,28 @@
 #ifndef _FILTER_SETTING_UTILITY_H__
 #define _FILTER_SETTING_UTILITY_H__
 
+#include <assert.h>
+
 inline
 const bool convert(const VARIANT_BOOL vb) {
-	if (VARIANT_TRUE == vb) return true;
-	else return false;
+	if (VARIANT_TRUE == vb) 
+		return true;
+	else if (VARIANT_FALSE == vb) 
+		return false;
+
+	assert(false);
+	return false;
 }
 
+inline 
+const VARIANT_BOOL convert(const bool value) {
+	if (true == value) {
+		return VARIANT_TRUE;
+	} else if (false == value) {
+		return VARIANT_FALSE;
+	}
+	assert(false);
+	return VARIANT_FALSE;
+}
 
 #endif  // _FILTER_SETTING_UTILITY_H__
