5bb72889c525ef5c484e029043c74b58c4f4d1bc
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-19 09:56:18 +0000

    1. remove the functionn : initialize, XMLConfiguration will initialize the class's object. 2. add a new function named defaultSetting, it the object was not initialized, there's default setting at least. 3. add two function enable and isEnable, and adjust all other relative functions.

--- trunk/libproject/Setting/WebHistoryRecorderSetting.cpp
+++ trunk/libproject/Setting/WebHistoryRecorderSetting.cpp
@@ -2,14 +2,64 @@
 #include "webhistoryrecordersetting.h"
 
 WebHistoryRecorderSetting::WebHistoryRecorderSetting(void) {
+	defaultSetting();
 }
 
 WebHistoryRecorderSetting::~WebHistoryRecorderSetting(void) {
 }
 
+void WebHistoryRecorderSetting::defaultSetting() {
+	enabled_ = true;
 
-// ==================================
-//
+	recordPornPage_ = true;
+	recordPornImage_ = true;
+	recordPornWebsite_ = true;
 
-void WebHistoryRecorderSetting::initialize() {
+	recordImage_ = false;
+	recordPage_ = false;
+	recordWebsite_ = false;
+
+}
+
+bool WebHistoryRecorderSetting::recordAllPages() const {
+	if (isEnable() ) {
+		return recordPage_;
+	} else {
+		return false;
+	}
+}
+bool WebHistoryRecorderSetting::recordPornPages() const	{
+	if (isEnable() ) {
+		return recordPornPage_;
+	} else {
+		return false;
+	}
+}
+bool WebHistoryRecorderSetting::recordPornImage() const	{
+	if (isEnable() ) {
+		return recordImage_;
+	} else {
+		return false;
+	}
+}
+bool WebHistoryRecorderSetting::recordAllImage() const	{
+	if (isEnable() ) {
+		return recordPornImage_;
+	} else {
+		return false;
+	}
+}
+bool WebHistoryRecorderSetting::recordAllWebsite() const {
+	if (isEnable() ) {
+		return recordWebsite_;
+	} else {
+		return false;
+	}
+}
+bool WebHistoryRecorderSetting::recordPornWebsite() const {
+	if (isEnable() ) {
+		return recordPornWebsite_;
+	} else {
+		return false;
+	}
 }
