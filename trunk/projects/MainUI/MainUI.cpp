f757fee8af3b68034ad0f6d5197bfcc5808d85cb
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-22 14:05:03 +0000

    1. remove some useless code 2. format the code.

--- trunk/projects/MainUI/MainUI.cpp
+++ trunk/projects/MainUI/MainUI.cpp
@@ -8,7 +8,7 @@
 #include ".\mainui.h"
 
 IDNSSetting *g_dnssetting = NULL;
-
+XMLConfiguration g_configuration;	// 配置信息
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -24,10 +24,7 @@ END_MESSAGE_MAP()
 
 // CMainUIApp 构造
 
-CMainUIApp::CMainUIApp()
-{
-	// TODO: 在此处添加构造代码，
-	// 将所有重要的初始化放置在 InitInstance 中
+CMainUIApp::CMainUIApp() {
 }
 
 
@@ -50,20 +47,14 @@ BOOL CMainUIApp::InitInstance()
 	// 使用 ComCtl32.dll 版本 6 或更高版本来启用可视化方式，
 	//则需要 InitCommonControls()。否则，将无法创建窗口。
 	InitCommonControls();
-
 	CWinApp::InitInstance();
-
 	AfxEnableControlContainer();
 
-	// 标准初始化
-	// 如果未使用这些功能并希望减小
-	// 最终可执行文件的大小，则应移除下列
-	// 不需要的特定初始化例程
-	// 更改用于存储设置的注册表项
-	// TODO: 应适当修改该字符串，
-	// 例如修改为公司或组织名
-	SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
+	// 读取配置信息
+	g_configuration.setInstance(AfxGetInstanceHandle());
+	g_configuration.initialize();
 
+	//============================================
 	CMainUIDlg dlg;
 	m_pMainWnd = &dlg;
 	INT_PTR nResponse = dlg.DoModal();
