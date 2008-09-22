97590457ebe192edc30ae760d8521bbac72b03c0
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-09-22 14:02:53 +0000

    1. remove the CGui* control. 2. initialize the state with XMLconfig

--- trunk/projects/MainUI/DlgSearchRule.cpp
+++ trunk/projects/MainUI/DlgSearchRule.cpp
@@ -5,12 +5,14 @@
 #include "MainUI.h"
 #include "DlgSearchRule.h"
 #include "globalvariable.h"
+#include ".\dlgsearchrule.h"
 
 // CDlgSearchRule 对话框
 
 IMPLEMENT_DYNAMIC(CDlgSearchRule, CDialog)
 CDlgSearchRule::CDlgSearchRule(CWnd* pParent /*=NULL*/)
 	: CBaseDlg(CDlgSearchRule::IDD, pParent)
+	, m_bEnableSearchRule(TRUE)
 {
 }
 
@@ -40,11 +42,32 @@ void CDlgSearchRule::DoDataExchange(CDataExchange* pDX)
 	CDialog::DoDataExchange(pDX);
 	DDX_Control(pDX, IDC_SEARCH_LIST, ListBox);
 	DDX_Control(pDX, IDC_CHK_ENABLE_SEARCH, m_chkEnableSearchChk);
+	DDX_Check(pDX, IDC_CHK_ENABLE_SEARCH, m_bEnableSearchRule);
 }
 
+void CDlgSearchRule::initializeData() {
+	g_configuration.getSearchRule()->enumBlackWord((Enumerator1<std::string>*)this);
+	m_bEnableSearchRule = g_configuration.getSearchRule()->isEnabled();
+
+	UpdateData(FALSE);
+}
+
+int CDlgSearchRule::Enum(const std::string &words) {
+	ListBox.AddItem(words.c_str());
+	return 0;
+}
 
 BEGIN_MESSAGE_MAP(CDlgSearchRule, CDialog)
 END_MESSAGE_MAP()
 
 
 // CDlgSearchRule 消息处理程序
+
+BOOL CDlgSearchRule::OnInitDialog()
+{
+	CBaseDlg::OnInitDialog();
+
+	initializeData();
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 异常: OCX 属性页应返回 FALSE
+}
--- trunk/projects/MainUI/DlgSearchRule.h
+++ trunk/projects/MainUI/DlgSearchRule.h
@@ -3,11 +3,13 @@
 #include <Guilib1.5\GuiListEdit.h>
 #include <Guilib1.5\GuiDropDownEdit.h>
 #include <Guilib1.5\GuiCheckBox.h>
+#include <Enumerate.h>
+#include <string>
 #include "afxwin.h"
 
 // CDlgSearchRule 对话框
 
-class CDlgSearchRule : public CBaseDlg
+class CDlgSearchRule : public CBaseDlg, Enumerator1<std::string> 
 {
 	DECLARE_DYNAMIC(CDlgSearchRule)
 
@@ -21,11 +23,20 @@ public:
 	virtual void OnRestore();
 	virtual void OnApply();
 	virtual void OnShow();
+
+	virtual BOOL OnInitDialog();
 protected:
+	// 初始化初始设置
+	void initializeData();
+	virtual int Enum(const std::string &dns) ;  // member of DNSEnumerator
+
 	CGuiListEdit ListBox;
-	CGuiCheckBox m_chkEnableSearchChk;
+	CButton m_chkEnableSearchChk;
 
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
 	DECLARE_MESSAGE_MAP()
-	
+
+
+	// control variable
+	BOOL m_bEnableSearchRule;
 };
