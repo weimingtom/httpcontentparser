924ecabcab44d982eb762224238e12b27b32a3a7
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-10-04 11:30:44 +0000

    1. add an new function, which can test whether get the Eyecare Application open or not. 2. In function StartEyecare, first call the upper function, if the Eyecare has been open, we just return, otherwise start an new instance.

--- trunk/libproject/sysutility/sysutility.cpp
+++ trunk/libproject/sysutility/sysutility.cpp
@@ -1,19 +1,30 @@
 #include "stdafx.h"
 #include "sysutility.h"
-#include <assert.h>
 #include <utility\strutility.h>
+#include <windowtitle.h>
+#include <assert.h>
+#include <process.h>
 
+
+#define SCREEN_SAVE_TYPE	TEXT("jpg")
 namespace {
 	void GetScreenRecordDir(TCHAR *moduleDir, const int len, HMODULE hModule);	//
 	void GetImageRecordDir(TCHAR *moduleDir, const int len, HMODULE hModule);	// 保存图像的目录
 	void GenerateImageFile(TCHAR *file, const int len, HMODULE hModule);	// 自动生成文件名
 	void GetHistoryRecordDir(TCHAR *moduleDir, const int len, HMODULE hModule);
 	void GenerateFullPath(TCHAR *fullpath, const int len, const TCHAR * dir, const TCHAR * filename);
+	void GetFilespathInDir(const TCHAR * dir, std::vector<strutility::_tstring> * files);
 };
 
 
 
 void StartEyecare(HMODULE hModule) {
+	// 首先检测应用程序是否已经打开了
+	HWND hOld = GetEyecareApp();
+	if (NULL != hOld) {
+		return;
+	}
+
 	TCHAR install_path[MAX_PATH], fullpath[MAX_PATH];
 	GetInstallPath(install_path, MAX_PATH, hModule);
 
@@ -30,7 +41,10 @@ DWORD GenScreenSPFile(TCHAR *fullpath, const int len, HMODULE hModule) {
 	SYSTEMTIME time;
 	GetLocalTime(&time);
 	TCHAR filename[MAX_PATH];
-	_sntprintf(filename, MAX_PATH, TEXT("%02d%02d%04d%02d%02d%02d.jpg"), time.wMonth, time.wDay,time.wYear, time.wHour, time.wMinute, time.wSecond);
+	_sntprintf(filename, MAX_PATH, TEXT("%02d%02d%04d%02d%02d%02d.%s"), 
+		time.wMonth, time.wDay,time.wYear, 
+		time.wHour, time.wMinute, time.wSecond,
+		SCREEN_SAVE_TYPE);
 
 	// 获取到目录
 	_sntprintf(fullpath, MAX_PATH, TEXT("%s%s"), dir, filename);
@@ -46,6 +60,18 @@ void ClearHistory(HMODULE hModule) {
 void ClearScreen(HMODULE hModule) {
 	TCHAR dir[MAX_PATH];
 	GetScreenRecordDir(dir, MAX_PATH, hModule);
+	
+	// 获取所有文件
+	using namespace std;
+	using namespace strutility;
+
+	vector<_tstring> vecfilepaths;
+	GetFilespathInDir(dir, &vecfilepaths);
+
+	vector<_tstring>::iterator iter = vecfilepaths.begin();
+	for (; iter != vecfilepaths.end(); ++iter) {
+		DeleteFile(iter->c_str());
+	}
 }
 
 const TCHAR * GetAppConfigFilename(TCHAR *fullpath, const int len, HMODULE hModule) {
@@ -103,6 +129,11 @@ const TCHAR * GetInstallPath(TCHAR *install_path, const int len, HMODULE hModule
 	return install_path;
 }
 
+// 当前的Eyecare是否在运行
+HWND GetEyecareApp() {
+	HWND hwnd = FindWindow(EYECARE_MAIN_WND_CLASS, NULL);
+	return hwnd;
+}
 //////////////////////////////////////////////////
 // utility functions
 
@@ -131,6 +162,25 @@ void GetHistoryRecordDir(TCHAR *historyDir, const int len, HMODULE hModule) {
 		_tmkdir(historyDir);
 }
 
+void GetFilespathInDir(const TCHAR * dir, std::vector<strutility::_tstring> * files) {
+	using namespace std;
+	using namespace strutility;
 
+	TCHAR fileexp[MAX_PATH];
+	_sntprintf(fileexp, MAX_PATH, "%s*.%s", dir, SCREEN_SAVE_TYPE);
+	WIN32_FIND_DATA find_data;
+	HANDLE hFind = FindFirstFile(fileexp, &find_data);
+
+	if (INVALID_HANDLE_VALUE == hFind) {
+		return;
+	}
+
+	// 记得将第一个文件添加进去
+	do {
+		files->push_back(find_data.cFileName);
+	} while (FindNextFile(hFind, &find_data));
+
+	FindClose(hFind);
+}
 
 };
