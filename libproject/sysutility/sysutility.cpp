08255d601c96a8df8f503697a6be478516da48d7
ynkhpp <ynkhpp@gmail.com>
2008-10-04 07:01:56 +0000

    1. change the screen record's file type, from bmp to jpeg. 2. add some utility function.

--- libproject/sysutility/sysutility.cpp
+++ libproject/sysutility/sysutility.cpp
@@ -1,13 +1,14 @@
 #include "stdafx.h"
 #include "sysutility.h"
 #include <assert.h>
-
+#include <utility\strutility.h>
 
 namespace {
 	DWORD GetScreenRecordDir(TCHAR *moduleDir, const int len, HMODULE hModule);	//
 	DWORD GetImageRecordDir(TCHAR *moduleDir, const int len, HMODULE hModule);	// 保存图像的目录
 	DWORD GenerateImageFile(TCHAR *file, const int len, HMODULE hModule);	// 自动生成文件名
 	DWORD GetHistoryRecordDir(TCHAR *moduleDir, const int len, HMODULE hModule);
+	DWORD GenerateFullPath(TCHAR *fullpath, const int len, const TCHAR * dir, const TCHAR * filename);
 };
 
 
@@ -29,7 +30,7 @@ DWORD GenScreenSPFile(TCHAR *fullpath, const int len, HMODULE hModule) {
 	SYSTEMTIME time;
 	GetLocalTime(&time);
 	TCHAR filename[MAX_PATH];
-	_sntprintf(filename, MAX_PATH, TEXT("%02d%02d%04d%02d%02d%02d.bmp"), time.wMonth, time.wDay,time.wYear, time.wHour, time.wMinute, time.wSecond);
+	_sntprintf(filename, MAX_PATH, TEXT("%02d%02d%04d%02d%02d%02d.jpg"), time.wMonth, time.wDay,time.wYear, time.wHour, time.wMinute, time.wSecond);
 
 	// 获取到目录
 	_sntprintf(fullpath, MAX_PATH, TEXT("%s%s"), dir, filename);
@@ -43,10 +44,8 @@ void ClearHistory(HMODULE hModule) {
 }
 
 void ClearScreen(HMODULE hModule) {
-	TCHAR dir[MAX_PATH], arg1 [MAX_PATH];
+	TCHAR dir[MAX_PATH];
 	GetScreenRecordDir(dir, MAX_PATH, hModule);
-	_sntprintf(arg1, MAX_PATH , TEXT("%s*"), dir);
-	_execlp(TEXT("del"), arg1, TEXT("/q"));
 }
 
 const TCHAR * GetAppConfigFilename(TCHAR *fullpath, const int len, HMODULE hModule) {
@@ -54,41 +53,45 @@ const TCHAR * GetAppConfigFilename(TCHAR *fullpath, const int len, HMODULE hModu
 	GetModuleFileName((HMODULE)hModule, filename, MAX_PATH);
 	GetFileNameDir(filename, directory, MAX_PATH);
 	assert (_tcslen(directory) != 0);
-	_sntprintf(fullpath, len, "%s\\%s", directory, CONFIG_FILE_NAME);
+	GenerateFullPath(fullpath, len, directory, CONFIG_FILE_NAME);
 	return fullpath;
 }
 
 const TCHAR* GetFileNameDir(const TCHAR *filename, TCHAR *directory, const unsigned len) {
 	TCHAR dir[MAX_PATH], driver[MAX_PATH];
 	_tsplitpath(filename, driver, dir, NULL, NULL);
-	_sntprintf(directory, len, TEXT("%s\\%s"), driver, dir);
+	_sntprintf(directory, len, TEXT("%s%s"), driver, dir);
 	return directory;
 }
 
 
 const TCHAR * GetPageDirectory(TCHAR * filename, const unsigned len, const TCHAR * installPath) {
 	assert (filename != NULL);
-	_sntprintf(filename, MAX_PATH, TEXT("%s\\%s"), installPath, TEXT("History\\text\\"));
+	assert ( true == strutility::endwith(installPath, TEXT("\\")));
+	GenerateFullPath(filename, MAX_PATH, installPath, TEXT("History\\text\\"));
 	return filename;
 }
 
 const TCHAR * GetImageDirectory(TCHAR * filename, const unsigned len, const TCHAR * installPath) {
 	assert (filename != NULL);
-	_sntprintf(filename, MAX_PATH, TEXT("%s\\%s"), installPath, TEXT("History\\images\\"));
+	assert ( true == strutility::endwith(installPath, "\\"));
+	GenerateFullPath(filename, MAX_PATH, installPath, TEXT("History\\images\\"));
 	return filename;
 }
 
 
 const TCHAR * GetWebSiteRecordPath(TCHAR *filename, const unsigned len, const TCHAR * installPath) {
 	assert (filename != NULL);
-	_sntprintf(filename, MAX_PATH, TEXT("%s\\%s"), installPath, TEXT("History\\websites\\websites.xml"));
+	assert ( true == strutility::endwith(installPath, "\\"));
+	GenerateFullPath(filename, MAX_PATH, installPath, TEXT("History\\websites\\websites.xml"));
 	return filename;
 }
 
 // 获取记录图片 文字历史的文件的路径
 const TCHAR * GetRecordConfigfile(TCHAR *filename, const unsigned len, const TCHAR * installPath) {
 	assert (filename != NULL);
-	_sntprintf(filename, MAX_PATH, TEXT("%s\\%s"), installPath, TEXT("History\\config.xml"));
+	assert (strutility::endwith(installPath, "\\") == true);
+	GenerateFullPath(filename, MAX_PATH, installPath, TEXT("History\\config.xml"));
 	return filename;
 }
 
@@ -105,11 +108,17 @@ const TCHAR * GetInstallPath(TCHAR *install_path, const int len, HMODULE hModule
 
 namespace {
 
+DWORD GenerateFullPath(TCHAR *fullpath, const int len, const TCHAR * dir, const TCHAR * filename) {
+	assert(strutility::endwith(dir, "\\") == true);
+	_sntprintf(fullpath, len, TEXT("%s%s"), dir, filename);
+	return _tcslen(fullpath);
+}
+
 // 保存屏幕截图的目录
 DWORD GetScreenRecordDir(TCHAR *screendir, const int len, HMODULE hModule) {
 	TCHAR moduleDir[MAX_PATH];
 	GetInstallPath(moduleDir, len, hModule);
-	_sntprintf(screendir, MAX_PATH, TEXT("%s%s\\"), moduleDir,TEXT("Screen\\"));
+	_sntprintf(screendir, MAX_PATH, TEXT("%s%s"), moduleDir,TEXT("Screen\\"));
 	if (_tchdir(screendir) == -1)
 		_tmkdir(screendir);
 
@@ -120,7 +129,7 @@ DWORD GetScreenRecordDir(TCHAR *screendir, const int len, HMODULE hModule) {
 DWORD GetHistoryRecordDir(TCHAR *historyDir, const int len, HMODULE hModule) {
 	TCHAR moduleDir[MAX_PATH];
 	GetInstallPath(moduleDir, len, hModule);
-	_sntprintf(historyDir, MAX_PATH, TEXT("%s%s\\"), moduleDir,TEXT("History\\"));
+	_sntprintf(historyDir, MAX_PATH, TEXT("%s%s"), moduleDir,TEXT("History\\"));
 	if (_tchdir(historyDir) == -1)
 		_tmkdir(historyDir);
 
