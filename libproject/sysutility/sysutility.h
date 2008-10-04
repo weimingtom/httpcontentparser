f3f8b9c3e335a7ebe565f953d65c041fc56b0c65
ynkhpp <ynkhpp@gmail.com>
2008-10-04 11:30:44 +0000

    1. add an new function, which can test whether get the Eyecare Application open or not. 2. In function StartEyecare, first call the upper function, if the Eyecare has been open, we just return, otherwise start an new instance.

--- libproject/sysutility/sysutility.h
+++ libproject/sysutility/sysutility.h
@@ -37,4 +37,7 @@ const TCHAR * GetWebSiteRecordPath(TCHAR *filename, const unsigned len, const TC
 
 // 获取历史的文件
 const TCHAR * GetRecordConfigfile(TCHAR *filename, const unsigned len, const TCHAR * installPath);
+
+// 当前的Eyecare是否在运行
+HWND GetEyecareApp();
 #endif  // _FILTER_SETTING_SYS_UTILITY_H__
