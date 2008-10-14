312809330a2bc5c20218af6b40420894e38856fe
ynkhpp <ynkhpp@gmail.com>
2008-10-14 02:56:03 +0000

    add and implement SeachRule

--- projects/FilterSetting/resource.h
+++ projects/FilterSetting/resource.h
@@ -11,6 +11,7 @@
 #define IDR_SCREENSAVE                  106
 #define IDR_WEBHISTORYRECORDER          107
 #define IDR_ACCESSNETWORK               108
+#define IDR_SEARCHRULE                  109
 
 // Next default values for new objects
 // 
@@ -19,6 +20,6 @@
 #define _APS_NEXT_RESOURCE_VALUE        201
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         201
-#define _APS_NEXT_SYMED_VALUE           109
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
