b7f48516798909213f222e93f737a3ab4fe028b5
ynkhpp <ynkhpp@gmail.com>
2008-10-14 03:08:53 +0000

    add some test for interface ISeachRule::removeBlackSeachWorld

--- test/TestFilterSetting/ISeachRuleTest.cpp
+++ test/TestFilterSetting/ISeachRuleTest.cpp
@@ -13,6 +13,27 @@ ISeachRuleTest::~ISeachRuleTest(void)
 {
 }
 
+
+void ISeachRuleTest::TestRemove() {
+	AutoInitInScale auto_;
+	ISearchRule * seachrule = NULL;
+	CoCreateInstance(CLSID_SearchRule, NULL, CLSCTX_LOCAL_SERVER, IID_ISearchRule, (LPVOID*)&seachrule);
+	
+	VARIANT_BOOL passed;
+	seachrule->addBlackSeachword(_bstr_t("xxxxxxxxxxxxxxx"));
+	seachrule->enableCheckSeachEngine(_bstr_t("google.com"), VARIANT_TRUE);
+	seachrule->enableCheckSeachEngine(_bstr_t("baidu.com"), VARIANT_FALSE);
+	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("seach.google.com"), &passed);
+	CPPUNIT_ASSERT (VARIANT_FALSE == passed);
+	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("www.baidu.com"),&passed);
+	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
+
+	seachrule->removeBlackSeachWord(_bstr_t("xxxxxxxxxxxxxxx"));
+	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("www.baidu.com"),&passed);
+	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
+	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("seach.google.com"), &passed);
+	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
+}
 void ISeachRuleTest::TestISeachRule() {
 	AutoInitInScale auto_;
 	ISearchRule * seachrule = NULL;
