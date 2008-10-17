08e0c0e7a4edcf5736fd4d9b3b6bffc2025e78df
ynkhpp <ynkhpp@gmail.com>
2008-10-17 02:21:09 +0000

    add an hotkey demo in others

---
+++ others/HotKeyTest/HotKeyTestDlg.cpp
@@ -0,0 +1,260 @@
+// HotKeyTestDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "HotKeyTest.h"
+#include "HotKeyTestDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialog
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+	//{{AFX_DATA(CAboutDlg)
+	enum { IDD = IDD_ABOUTBOX };
+	//}}AFX_DATA
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CAboutDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	//{{AFX_MSG(CAboutDlg)
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
+{
+	//{{AFX_DATA_INIT(CAboutDlg)
+	//}}AFX_DATA_INIT
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CAboutDlg)
+	//}}AFX_DATA_MAP
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
+	//{{AFX_MSG_MAP(CAboutDlg)
+		// No message handlers
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CHotKeyTestDlg dialog
+
+CHotKeyTestDlg::CHotKeyTestDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CHotKeyTestDlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CHotKeyTestDlg)
+	m_filename = _T("");
+	//}}AFX_DATA_INIT
+	// Note that LoadIcon does not require a subsequent DestroyIcon in Win32
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CHotKeyTestDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CHotKeyTestDlg)
+	DDX_Control(pDX, IDC_BUTTON2, m_stop);
+	DDX_Control(pDX, IDC_BUTTON1, m_start);
+	DDX_Control(pDX, IDC_COMBO2, m_virkey);
+	DDX_Control(pDX, IDC_COMBO1, m_modifier);
+	DDX_Text(pDX, IDC_EDIT1, m_filename);
+	//}}AFX_DATA_MAP
+}
+
+BEGIN_MESSAGE_MAP(CHotKeyTestDlg, CDialog)
+	//{{AFX_MSG_MAP(CHotKeyTestDlg)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, OnStart)
+	ON_BN_CLICKED(IDC_BUTTON2, OnStop)
+	ON_WM_DESTROY()
+	ON_MESSAGE(WM_HOTKEY,OnHotKey)
+	ON_BN_CLICKED(IDC_BUTTON3, OnBrowse)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CHotKeyTestDlg message handlers
+
+BOOL CHotKeyTestDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		CString strAboutMenu;
+		strAboutMenu.LoadString(IDS_ABOUTBOX);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+	
+	// TODO: Add extra initialization here
+	m_modifier.InsertString(0,"MOD_ALT");
+	m_modifier.InsertString(0,"MOD_CONTROL");
+	m_modifier.InsertString(0,"MOD_SHIFT");
+	m_modifier.InsertString(0,"MOD_WIN");
+
+	m_modifier.SetCurSel(0);
+
+	for(int i='Z';i>='A';i--)
+		m_virkey.InsertString(0,(CString)(char)i);
+
+	m_virkey.SetCurSel(0);
+
+	m_stop.EnableWindow(false);
+
+	
+	
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CHotKeyTestDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialog::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CHotKeyTestDlg::OnPaint() 
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, (WPARAM) dc.GetSafeHdc(), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialog::OnPaint();
+	}
+}
+
+// The system calls this to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CHotKeyTestDlg::OnQueryDragIcon()
+{
+	return (HCURSOR) m_hIcon;
+}
+
+void CHotKeyTestDlg::OnStart() 
+{
+	// TODO: Add your control notification handler code here
+	UINT modifier;
+	CString modifierstring;
+	m_modifier.GetLBText(m_modifier.GetCurSel(),modifierstring);
+	
+	if(modifierstring=="MOD_ALT")
+		modifier=MOD_ALT;
+	else if (modifierstring=="MOD_CONTROL")
+		modifier=MOD_CONTROL;
+	else if(modifierstring=="MOD_SHIFT")
+		modifier=MOD_SHIFT;
+	else if(modifierstring=="MOD_WIN")
+		modifier=MOD_WIN;
+
+	CString vkeystr;
+	m_virkey.GetLBText(m_virkey.GetCurSel(),vkeystr);
+
+	if(RegisterHotKey(this->m_hWnd,100,modifier,vkeystr[0]))
+	{	
+		m_start.EnableWindow(false);
+		m_stop.EnableWindow(true);
+	}
+	else
+		MessageBox("RegisterHotKey failed");
+}
+
+void CHotKeyTestDlg::OnStop() 
+{
+	// TODO: Add your control notification handler code here
+	UnregisterHotKey(this->m_hWnd,100);	
+	m_stop.EnableWindow(false);
+	m_start.EnableWindow(true);
+	
+}
+
+LRESULT CHotKeyTestDlg::OnHotKey(WPARAM wParam, LPARAM lParam)
+{
+
+	ShellExecute(this->m_hWnd,"open",m_filename,
+		NULL,NULL,SW_SHOW);
+	return 0;
+}
+
+void CHotKeyTestDlg::OnDestroy() 
+{
+	CDialog::OnDestroy();
+	
+	// TODO: Add your message handler code here
+	UnregisterHotKey(this->m_hWnd,100);
+	
+}
+
+void CHotKeyTestDlg::OnBrowse() 
+{
+	CFileDialog m_fdlg(true,NULL,NULL,OFN_HIDEREADONLY,
+		"Exe Files (*.exe)|*.exe||");
+	m_fdlg.m_ofn.lpstrInitialDir = "C:\\";
+	if(m_fdlg.DoModal()==IDOK)
+	{
+		m_filename = m_fdlg.GetPathName();
+		UpdateData(false);
+	}
+}
