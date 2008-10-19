e32383696b6f7c7bd65234ff78c863221c5cb650
ynkhpp <ynkhpp@gmail.com>
2008-10-19 06:19:39 +0000

    implement the function sethotky

--- projects/MainUI/DlgOptions.cpp
+++ projects/MainUI/DlgOptions.cpp
@@ -5,12 +5,13 @@
 #include "MainUI.h"
 #include "DlgOptions.h"
 #include ".\dlgoptions.h"
-#include <hotkey.h>
+#include <app_constants.h>
 #include <sysutility.h>
 #include <comdef.h>
+#include <com\comutility.h>
 #include <com\FilterSetting_i.c>
 #include <com\FilterSetting.h>
-
+#include <typeconvert.h>
 
 // CDlgOptions 对话框
 
@@ -19,11 +20,45 @@ CDlgOptions::CDlgOptions(CWnd* pParent /*=NULL*/)
 	: CBaseDlg(CDlgOptions::IDD, pParent)
 	, m_bAutoRun(FALSE)
 {
+	m_wLaunchKey = 0;
+	m_wLaunchModifier = 0;
+	m_wMainUIKey = 0;
+	m_wMainUIModifier = 0;
+	m_wSwitchUserKey = 0;
+	m_wSwitchUserModifier = 0;
 }
 
 CDlgOptions::~CDlgOptions()
 {
 }
+
+namespace {
+// 注意API的HOTKEY与MFC定义的值不相同，真是晕菜
+WORD  getModifierKey(WORD kModify_mfc) {
+	WORD vModifiers  = 0;
+	if (kModify_mfc & HOTKEYF_SHIFT)
+		vModifiers |= MOD_SHIFT;
+	if (kModify_mfc & HOTKEYF_CONTROL)
+		vModifiers |= MOD_CONTROL;
+	if (kModify_mfc & HOTKEYF_ALT)
+		vModifiers |= MOD_ALT;
+
+	return vModifiers;
+}
+
+BOOL setLaunchHotkey(WORD vKey, WORD vModifiers) {
+	try {
+		AutoInitInScale auto_init_com;
+		VARIANT_BOOL bSucc;
+		IAppSetting *app = NULL;
+		CoCreateInstance(CLSID_AppSetting, NULL, CLSCTX_LOCAL_SERVER, IID_IAppSetting, (LPVOID*)&app);
+		app->setHotkey(vKey, vModifiers, HOTKEY_LANUCH_MAINUI, &bSucc);
+		return convert(bSucc);
+	} catch(...) {
+		return FALSE;
+	}
+}
+};
 
 void CDlgOptions::DoDataExchange(CDataExchange* pDX)
 {
@@ -35,27 +70,39 @@ void CDlgOptions::DoDataExchange(CDataExchange* pDX)
 	DDX_Control(pDX, IDC_HOTKEY_SHOWDLG, m_hotKeyShowDlg);
 	DDX_Control(pDX, IDC_HOTKEY_SWITCHUSR, m_hotkeySwitchUser);
 	DDX_Check(pDX, IDC_CHK_AUTOLOAD, m_bAutoRun);
+	DDX_Control(pDX, IDC_HOTKEY_LAUNCH, m_hotkeyLaunch);
 }
 
 void CDlgOptions::setHotKey() {
 	CString strPrompt;
 	strPrompt.LoadString(IDS_HOTKEY_CONFLICT);
 
+	// 注册热键
+	WORD vModifiers_mfc;
+
 	// 设置显示对话框的热键
-	//WORD vKey, vModifiers;
-	//m_hotKeyShowDlg.GetHotKey(vKey, vModifiers);
+	m_hotKeyShowDlg.GetHotKey(m_wMainUIKey, vModifiers_mfc);
+	m_wMainUIModifier = getModifierKey(vModifiers_mfc);
+	if (!RegisterHotKey(AfxGetMainWnd()->GetSafeHwnd() ,HOTKEY_SHOW_MAINUI, m_wMainUIModifier,m_wMainUIKey)) {
+		MessageBox(strPrompt);
+		return;
+	}
 
-	//if (!RegisterHotKey(AfxGetMainWnd()->GetSafeHwnd() ,100,vModifiers,vKey)) {
-	//	
-	//	MessageBox(strPrompt);
-	//	return;
-	//}
+	// 设置显示对话框的热键
+	m_hotkeySwitchUser.GetHotKey(m_wSwitchUserKey, vModifiers_mfc);
+	m_wSwitchUserModifier = getModifierKey(vModifiers_mfc);
+	if (!RegisterHotKey(AfxGetMainWnd()->GetSafeHwnd(), HOTKEY_SHOW_SWITCH_USER, m_wSwitchUserModifier, m_wSwitchUserKey)) {
+		MessageBox(strPrompt);
+		return;
+	}
 
-	//m_hotkeySwitchUser.GetHotKey(vKey, vModifiers);
-	//if (!RegisterHotKey(AfxGetMainWnd()->GetSafeHwnd(),100,vModifiers,vKey)) {
-	//	MessageBox(strPrompt);
-	//	return;
-	//}
+	// 设置启动程序， 必须通过COM设置
+	m_hotkeyLaunch.GetHotKey(m_wLaunchKey, vModifiers_mfc);
+	m_wLaunchModifier = getModifierKey(vModifiers_mfc);
+	if (setLaunchHotkey(m_wLaunchKey, m_wLaunchModifier)) {
+		MessageBox(strPrompt);
+		return;
+	}
 }
 
 
@@ -65,7 +112,13 @@ int CDlgOptions::OnApply() {
 	return 0;
 }
 
-void CDlgOptions::restoreSetting() {
+void CDlgOptions::restoreSetting() {	
+	// 热键
+	m_hotkeyLaunch.SetHotKey(m_wLaunchKey, m_wLaunchModifier);
+	m_hotKeyShowDlg.SetHotKey(m_wMainUIKey, m_wMainUIModifier);
+	m_hotkeySwitchUser.SetHotKey(m_wSwitchUserKey, m_wSwitchUserModifier);
+
+	// 自动运行
 	m_bAutoRun = isAutoRun((HMODULE)AfxGetInstanceHandle());
 	m_bOld_autorun = m_bAutoRun;
 	UpdateData(FALSE);
