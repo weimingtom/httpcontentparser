f1964fdb92fba3b8cfd92f8a051493691c753ecb
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-10-21 03:10:16 +0000

    reface, just rename some variable.

--- trunk/test/TestFilterSetting/ISeachRuleTest.cpp
+++ trunk/test/TestFilterSetting/ISeachRuleTest.cpp
@@ -19,19 +19,20 @@ void ISeachRuleTest::TestRemove() {
 	ISearchRule * seachrule = NULL;
 	CoCreateInstance(CLSID_SearchRule, NULL, CLSCTX_LOCAL_SERVER, IID_ISearchRule, (LPVOID*)&seachrule);
 	
+	_bstr_t test_string("xxxxxxxxxxxxxxx");
 	VARIANT_BOOL passed;
-	seachrule->addBlackSeachword(_bstr_t("xxxxxxxxxxxxxxx"));
+	seachrule->addBlackSeachword(test_string);
 	seachrule->enableCheckSeachEngine(_bstr_t("google.com"), VARIANT_TRUE);
 	seachrule->enableCheckSeachEngine(_bstr_t("baidu.com"), VARIANT_FALSE);
-	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("seach.google.com"), &passed);
+	seachrule->check(test_string,  _bstr_t("seach.google.com"), &passed);
 	CPPUNIT_ASSERT (VARIANT_FALSE == passed);
-	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("www.baidu.com"),&passed);
+	seachrule->check(test_string,  _bstr_t("www.baidu.com"),&passed);
 	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
 
-	seachrule->removeBlackSeachWord(_bstr_t("xxxxxxxxxxxxxxx"));
-	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("www.baidu.com"),&passed);
+	seachrule->removeBlackSeachWord(test_string);
+	seachrule->check(test_string,  _bstr_t("www.baidu.com"),&passed);
 	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
-	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("seach.google.com"), &passed);
+	seachrule->check(test_string,  _bstr_t("seach.google.com"), &passed);
 	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
 }
 void ISeachRuleTest::TestISeachRule() {
@@ -39,27 +40,30 @@ void ISeachRuleTest::TestISeachRule() {
 	ISearchRule * seachrule = NULL;
 	CoCreateInstance(CLSID_SearchRule, NULL, CLSCTX_LOCAL_SERVER, IID_ISearchRule, (LPVOID*)&seachrule);
 	
+	_bstr_t test_string("xxxxxxxxxxxxxxx");
+	_bstr_t test_string_none("xxxxxxxxxxxxxxx___");
+
 	VARIANT_BOOL passed;
-	seachrule->addBlackSeachword(_bstr_t("xxxxxxxxxxxxxxx"));
+	seachrule->addBlackSeachword(test_string);
 	seachrule->enableCheckSeachEngine(_bstr_t("google.com"), VARIANT_TRUE);
 	seachrule->enableCheckSeachEngine(_bstr_t("baidu.com"), VARIANT_FALSE);
-	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("seach.google.com"), &passed);
+	seachrule->check(test_string,  _bstr_t("seach.google.com"), &passed);
 	CPPUNIT_ASSERT (VARIANT_FALSE == passed);
-	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"),  _bstr_t("www.baidu.com"),&passed);
+	seachrule->check(test_string,  _bstr_t("www.baidu.com"),&passed);
 	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
 
-	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx___"), _bstr_t("google.com"),&passed);
+	seachrule->check(test_string_none, _bstr_t("google.com"),&passed);
 	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
 
 	seachrule->enableCheckSeachEngine(_bstr_t("google.com"), VARIANT_FALSE);
 
-	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx"), _bstr_t("google.com"),&passed);
+	seachrule->check(test_string, _bstr_t("google.com"),&passed);
 	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
 
-	seachrule->check(_bstr_t("xxxxxxxxxxxxxxx___"), _bstr_t("google.com"),&passed);
+	seachrule->check(test_string_none, _bstr_t("google.com"),&passed);
 	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
 
-		seachrule->check(_bstr_t("xxxxxxxxxxxxxxx___"), _bstr_t("baidu.com"),&passed);
+		seachrule->check(test_string_none, _bstr_t("baidu.com"),&passed);
 	CPPUNIT_ASSERT (VARIANT_TRUE == passed);
 
 	SafeRelease(seachrule);
