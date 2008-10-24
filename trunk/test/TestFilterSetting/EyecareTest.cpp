d318951f66bdbf8af25017bc5e3cd7380445a8eb
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-10-24 14:35:46 +0000

    remove the test.

--- trunk/test/TestFilterSetting/EyecareTest.cpp
+++ trunk/test/TestFilterSetting/EyecareTest.cpp
@@ -11,16 +11,4 @@ EyecareTest::~EyecareTest(void) {
 }
 
 void EyecareTest::getTimeLeft() {
-	IEyecare *pEyeCare = NULL;
-
-	CoInitialize(NULL);
-	CoCreateInstance(CLSID_Eyecare, NULL, CLSCTX_ALL, IID_IEyecare, (LPVOID*)&pEyeCare);
-	pEyeCare->setEnterTime(10);
-	pEyeCare->setEyecareTime(10);
-
-	LONG timeLeft ;
-	pEyeCare->getTimeLeft(&timeLeft);
-
-	pEyeCare->Release();
-	CoUninitialize();
 }
