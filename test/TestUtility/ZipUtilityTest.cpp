fb8fdc36b7bc40de801c5265b6d4708646b7dd0c
ynkhpp <ynkhpp@gmail.com>
2008-10-29 07:43:44 +0000

    implement the zip utility class and add unit test for this class.
    refer issue 4

--- test/TestUtility/ZipUtilityTest.cpp
+++ test/TestUtility/ZipUtilityTest.cpp
@@ -1,6 +1,7 @@
 #include "StdAfx.h"
 #include ".\ziputilitytest.h"
 #include <utility/ZipUtility.h>
+#include <zlib/zlib.h>
 
 ZipUtilityTest::ZipUtilityTest(void)
 {
@@ -12,4 +13,26 @@ ZipUtilityTest::~ZipUtilityTest(void)
 
 
 void ZipUtilityTest::testZlibUtility() {
+	{
+	ZipUtility<5> zip, zip2;
+	char  source[] = "abcdefghj";
+	zip.compress((Bytef*)source, strlen(source));
+	// 使用标准方法压缩
+	
+	char buffer[1024];
+	zip2.uncompress(zip.get_buffer(), zip.get_data_size());
+	CPPUNIT_ASSERT(0 == memcmp(zip2.get_buffer(), source, strlen(source)));
+	}
+
+	{
+	ZipUtility<1024> zip, zip2;
+	char  source[] = "abcdefghj";
+	zip.compress((Bytef*)source, strlen(source));
+	// 使用标准方法压缩
+	
+	char buffer[1024];
+	zip2.uncompress(zip.get_buffer(), zip.get_data_size());
+	CPPUNIT_ASSERT(0 == memcmp(zip2.get_buffer(), source, strlen(source)));
+	}
+
 }
