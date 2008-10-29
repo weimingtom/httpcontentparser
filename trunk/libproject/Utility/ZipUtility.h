a8e9e8a9ef756643018a5a14b14888f2ac9c28de
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-10-29 07:43:44 +0000

    implement the zip utility class and add unit test for this class.
    refer issue 4

--- trunk/libproject/Utility/ZipUtility.h
+++ trunk/libproject/Utility/ZipUtility.h
@@ -13,6 +13,10 @@ class ZipUtility {
 public:
 	ZipUtility(void) {
 		heap_buffer_ = NULL;
+		have_ = 0;
+		buffer_size_ = STACK_BUFFER_SIZE;
+
+		buffer_pointer_ = stack_buffer_;
 	}
 	~ZipUtility(void) {
 		if (NULL != heap_buffer_) {
@@ -21,20 +25,96 @@ public:
 		}
 	}
 
-	int infloate_size(const char *src, const int size, int *infloat);
-	int unzip(const char * src, const int size, int *infatesize);
-private:
-	char * buffer_pointer_;
+public:
+	int infloate_size(Bytef *src, const int size);
+	int uncompress(Bytef * src, const int size);
+	int compress(Bytef *src, const int size, const int level = Z_DEFAULT_COMPRESSION);
 
-	char stack_buffer_[STACK_BUFFER_SIZE];
-	char *heap_buffer_;
+	// 缓冲区
+	int get_buffer_size() const { return buffer_size_;}
+	int get_inflate_size() const { return have_;}
+	int get_data_size() const { return have_;}
+	Bytef * get_buffer() { return buffer_pointer_;}
+	
+private:
+	Bytef * buffer_pointer_;
+
+	// buffer_pointer_会首先使用STACK上的缓冲
+	// 如果不够用，才会在堆上分配内容
+	Bytef stack_buffer_[STACK_BUFFER_SIZE];
+	Bytef *heap_buffer_;
+	int buffer_size_;
+	int have_;
+
+private:
+	// 重新分配缓冲区
+	int realloc_buffer() {
+		const int new_buffer_size = get_buffer_size() * 2;
+		Bytef * new_buffer = new Bytef[new_buffer_size];
+		memcpy(new_buffer, get_buffer(), get_buffer_size());
+
+		// 释放缓存去(如果不是栈缓冲区)
+		if (get_buffer_size() > STACK_BUFFER_SIZE) {
+			delete[] get_buffer();
+		}
+
+		// 充值指针
+		buffer_pointer_ = new_buffer;
+		buffer_size_ = new_buffer_size;
+
+		return get_buffer_size();
+	}
 };
 
-
+// 压缩
 template <int STACK_BUFFER_SIZE>
-int ZipUtility<STACK_BUFFER_SIZE>::infloate_size(const char * src, const int size, int *infloat) {
+int ZipUtility<STACK_BUFFER_SIZE>::compress(Bytef *src, const int size, const int level) {
+	int ret, flush = Z_FINISH;
+	z_stream strm;
+
+	/* allocate deflate state */
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.opaque = Z_NULL;
+	ret = deflateInit(&strm, level);
+	if (ret != Z_OK)
+		return ret;
+
+	strm.avail_in = size;
+	strm.next_in = src;
+
+	/* run deflate() on input until output buffer not full, finish
+	compression if all of source has been read in */
+	have_ = 0;
+	do {
+		strm.avail_out = get_buffer_size() - get_data_size();
+		strm.next_out = get_buffer() + get_data_size();
+		ret = deflate(&strm, flush);    /* no bad return value */
+		assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+		have_ += get_buffer_size() - get_data_size() - strm.avail_out;
+		
+		// 如果缓冲区用尽，就需要重新分配缓冲区
+		if (get_buffer_size() == have_) {
+			realloc_buffer();
+		}
+
+	} while (strm.avail_out == 0);
+
+    /* clean up and return */
+    (void)deflateEnd(&strm);
+    return Z_OK;
+
+}
+
+// 获取解压缩后的大小
+template <int STACK_BUFFER_SIZE>
+int ZipUtility<STACK_BUFFER_SIZE>::infloate_size(Bytef * src, const int size) {
+	have_ = 0; // 初试大小为0
 	int ret;
     z_stream strm;
+
+	if (size == 0)
+		return Z_OK;
 
     /* allocate inflate state */
     strm.zalloc = Z_NULL;
@@ -46,39 +126,30 @@ int ZipUtility<STACK_BUFFER_SIZE>::infloate_size(const char * src, const int siz
     if (ret != Z_OK)
         return ret;
 
-    /* decompress until deflate stream ends or end of file */
-    do {
-        strm.avail_in = size;
-        strm.next_in = src;
+	// 初始化
+	strm.avail_in = size;
+	strm.next_in = src;
 
-        /* run inflate() on input until output buffer not full */
-        do {
-            strm.avail_out = STACK_BUFFER_SIZE;
-            strm.next_out = stack_buffer_;
-            ret = inflate(&strm, Z_NO_FLUSH);
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            switch (ret) {
-            case Z_NEED_DICT:
-                ret = Z_DATA_ERROR;     /* and fall through */
-            case Z_DATA_ERROR:
-            case Z_MEM_ERROR:
-                (void)inflateEnd(&strm);
-                return ret;
-            }
-        } while (strm.avail_out == 0);
+	
+	do {
+		strm.avail_out = get_buffer_size() - get_data_size();
+		strm.next_out = get_buffer() + get_data_size();
 
-        /* done when inflate() says it's done */
-    } while (ret != Z_STREAM_END);
+		// 增加大小
+		have_ += STACK_BUFFER_SIZE - strm.avail_out;
+	} while (strm.avail_out == 0);
 
-    /* clean up and return */
-    (void)inflateEnd(&strm);
+	// 销毁
+	(void)inflateEnd(&strm);
 
 	*infloat = strm.total_out;
     return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
 }
 
+// 解压缩
 template <int STACK_BUFFER_SIZE>
-int ZipUtility<STACK_BUFFER_SIZE>::unzip(const char * src, const int size, int *infatesize) {
+int ZipUtility<STACK_BUFFER_SIZE>::uncompress(Bytef * src, const int size) {
+	have_ = 0;
 	int ret;
     z_stream strm;
 
@@ -88,36 +159,46 @@ int ZipUtility<STACK_BUFFER_SIZE>::unzip(const char * src, const int size, int *
     strm.opaque = Z_NULL;
     strm.avail_in = 0;
     strm.next_in = Z_NULL;
+
+	if (size == 0) 
+		return Z_OK;
+
+
     ret = inflateInit(&strm);
     if (ret != Z_OK)
         return ret;
 
-    /* decompress until deflate stream ends or end of file */
-    do {
-        strm.avail_in = size;
-        if (strm.avail_in == 0)
-            break;
-        strm.next_in = src;
+	// 开始解压缩
+	strm.avail_in = size;
+	strm.next_in = src;
 
-        /* run inflate() on input until output buffer not full */
-        do {
-            strm.avail_out = CHUNK;
-            strm.next_out = out;
-            ret = inflate(&strm, Z_NO_FLUSH);
+	
+	/* run inflate() on input until output buffer not full */
+	do {
+		strm.avail_out = get_buffer_size() - get_data_size();
+		strm.next_out = get_buffer() + get_data_size();
 
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            switch (ret) {
-            case Z_NEED_DICT:
-                ret = Z_DATA_ERROR;     /* and fall through */
-            case Z_DATA_ERROR:
-            case Z_MEM_ERROR:
-                (void)inflateEnd(&strm);
-                return ret;
-            }
-        } while (strm.avail_out == 0);
+		// 解压缩
+		ret = inflate(&strm, Z_NO_FLUSH);
 
-        /* done when inflate() says it's done */
-    } while (ret != Z_STREAM_END);
+		assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+		switch (ret) {
+			case Z_NEED_DICT:
+				ret = Z_DATA_ERROR;     /* and fall through */
+			case Z_DATA_ERROR:
+			case Z_MEM_ERROR:
+				(void)inflateEnd(&strm);
+				return ret;
+		}
+
+		// 解压缩所处理的数据大小
+		have_ += get_buffer_size() - get_data_size() - strm.avail_out;
+
+		// 如果缓冲区用尽，就需要重新分配缓冲区
+		if (get_buffer_size() == have_) {
+			realloc_buffer();
+		}
+	} while (strm.avail_out == 0);
 
     /* clean up and return */
     (void)inflateEnd(&strm);
