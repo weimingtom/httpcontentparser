9d4f888fa13d5b91c891aeb413cc70da4ef7d22c
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2008-12-15 08:49:13 +0000

    the contentcheck could read the setting from the xml file.

--- trunk/libproject/Setting/contentCheckSetting.cpp
+++ trunk/libproject/Setting/contentCheckSetting.cpp
@@ -46,6 +46,8 @@ bool ContentCheckSetting::needCheckBySize(const int size) const {
 
 	if (size > max_check_size_)
 		return false;
+
+	return true;
 }
 
 void ContentCheckSetting::defaultSetting() {
@@ -111,26 +113,35 @@ TiXmlElement * ContentCheckSetting::saveImageRule(TiXmlElement *root) {
 	item_png->SetAttribute(CONFIG_CONST_ENABLE, enabledFromBool(isSettingNeedCheck(IMAGE_TYPE_PNG)));
 	rule_root->LinkEndChild(item_png);
 
+	// scope
+	TCHAR buffer[MAX_PATH];
+	TiXmlElement *item_scope = new TiXmlElement(CONFIG_NODE_IMAGESCOPE);
+	item_scope->SetAttribute(CONFIG_CONST_ENABLE, enabledFromBool(enabledCheckBySize()));
+	item_scope->SetAttribute(CONFIG_CONST_MIN, _itot(min_check_size_, buffer, MAX_PATH));
+	item_scope->SetAttribute(CONFIG_CONST_MAX, _itot(max_check_size_, buffer, MAX_PATH));
+	rule_root->LinkEndChild(item_scope);
+
 	root->LinkEndChild(rule_root);
 	return rule_root;
 }
 
-
 //========================================================
 // 图片规则
-int ContentCheckSetting::setImageCheck(const TCHAR *imagetype, const TCHAR *enable) {
-	if (NULL == imagetype || NULL == enable)
+int ContentCheckSetting::setImageCheck(const TCHAR *node_name, const TCHAR *enable) {
+	if (NULL == node_name || NULL == enable)
 		return -1;
 
 	bool a = enabledFromString(enable);
-	if (0 == _tcscmp(imagetype, CONFIG_NODE_IMAGETYPE_JPG)) {
+	if (0 == _tcscmp(node_name, CONFIG_NODE_IMAGETYPE_JPG)) {
 		enableCheck(IMAGE_TYPE_JPEG, enabledFromString(enable));
-	} else if (0 == _tcscmp(imagetype, CONFIG_NODE_IMAGETYPE_BMP)) {
+	} else if (0 == _tcscmp(node_name, CONFIG_NODE_IMAGETYPE_BMP)) {
 		enableCheck(IMAGE_TYPE_BMP, enabledFromString(enable));
-	} else if (0 == _tcscmp(imagetype, CONFIG_NODE_IMAGETYPE_GIF)) {
+	} else if (0 == _tcscmp(node_name, CONFIG_NODE_IMAGETYPE_GIF)) {
 		enableCheck(IMAGE_TYPE_GIF, enabledFromString(enable));
-	} else if (0 == _tcscmp(imagetype, CONFIG_NODE_IMAGETYPE_PNG)) {
+	} else if (0 == _tcscmp(node_name, CONFIG_NODE_IMAGETYPE_PNG)) {
 		enableCheck(IMAGE_TYPE_PNG, enabledFromString(enable));
+	} else if (0 == _tcscmp(node_name, CONFIG_NODE_IMAGESCOPE)) {
+		enableCheckBySize(enabledFromString(enable));
 	} else {
 		assert (false);
 		return -1;
@@ -138,13 +149,15 @@ int ContentCheckSetting::setImageCheck(const TCHAR *imagetype, const TCHAR *enab
 	return 0;
 }
 
-
 int ContentCheckSetting::getImageRule(TiXmlElement * rule_root) {
 	TiXmlNode * node = rule_root->FirstChild();
 	while (NULL != node) {
 		TiXmlElement *ele = node->ToElement();
-		if (NULL != ele) {
+		if (NULL != ele && 0 == _tcscmp(ele->Value(), CONFIG_NODE_IMAGE_CHECK_ITEM)) {// 如果是check
 			setImageCheck(ele->Attribute(CONFIG_NODE_IMAGETYPE), ele->Attribute(CONFIG_CONST_ENABLE));
+		} else if (NULL != ele && 0 == _tcscmp(ele->Value(), CONFIG_NODE_IMAGESCOPE)) {// 如果是scope
+			enableCheckBySize(enabledFromString(ele->Attribute(CONFIG_CONST_ENABLE)));
+			setCheckScope(_ttoi(ele->Attribute(CONFIG_CONST_MIN)), _ttoi(ele->Attribute(CONFIG_CONST_MAX)));
 		}
 
 		node = node->NextSibling();
