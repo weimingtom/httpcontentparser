b68e90e0cba33d859997d52a303f27093440be5f
ynkhpp <ynkhpp@gmail.com>
2008-12-24 08:34:48 +0000

    add two new tests.

---
+++ libproject/Utility/ReplacePacket.h
@@ -0,0 +1,7 @@
+#ifndef _UTILITY_REPLACE_PACKET_H__
+#define _UTILITY_REPLACE_PACKET_H__
+
+class HTTPPacket;
+int FillBlankPacket(HTTPPacket *packet, HTTPPacket *new_packet);
+
+#endif  // _UTILITY_REPLACE_PACKET_H__
