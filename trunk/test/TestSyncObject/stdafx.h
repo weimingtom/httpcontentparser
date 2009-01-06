d05a75f625147d280ca1b26e1941c9aaebc07f30
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-01-06 05:46:51 +0000

    add a new project to test the syncobject.

---
+++ trunk/test/TestSyncObject/stdafx.h
@@ -0,0 +1,35 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是常用但不常更改的项目特定的包含文件
+//
+
+#pragma once
+
+
+
+#include <iostream>
+#include <tchar.h>
+#include <windows.h>
+#include <cppunit\extensions\HelperMacros.h>
+#include <cppunit\ui\text\TestRunner.h>
+#include <cppunit\extensions\TestFactoryRegistry.h>
+#include <cppunit\TestResult.h>
+#include <cppunit\TestResultCollector.h>
+#include <cppunit\BriefTestProgressListener.h>
+#include <cppunit\CompilerOutputter.h> 
+#include <objbase.h>
+#include <windows.h>
+#include <comdef.h>
+
+
+#ifdef _DEBUG
+#	pragma comment(lib, "cppunitd.lib")
+#	pragma comment(lib, "testrunnerd.lib")
+#	pragma comment(lib, "utilityd.lib")
+#else 
+#	pragma comment(lib, "TestRunner.lib")
+#	pragma comment(lib, "cppunit.lib")
+#	pragma comment(lib, "utilityd.lib")
+#endif
+
+
+#pragma comment(lib, "ws2_32.lib")
