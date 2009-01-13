f14225e0f7837de8942c70d0ee985d0d28a35daa
ynkhpp <ynkhpp@gmail.com>
2009-01-13 02:25:11 +0000

    1. remove the project SyncObject 2. place the SysMutex to proejct utility. 3. remove the FileMapping from the project.

--- libproject/Utility/syncutility.cpp
+++ libproject/Utility/syncutility.cpp
@@ -17,4 +17,38 @@ void CAutoCreateCS::unlock() {
 	LeaveCriticalSection(&cs_);
 
 }
+
+
+//============================
+// class CSysMutex
+CSysMutex::CSysMutex() {
+	hMutex_ = NULL;
+}
+CSysMutex::CSysMutex(const std::string &name) {
+	name_ = name;
+	hMutex_ = NULL;
+}
+
+CSysMutex::~CSysMutex(void) {
+	if (hMutex_ != NULL) {
+		CloseHandle(hMutex_);
+	}
+}
+
+void CSysMutex::Lock() {
+	::WaitForSingleObject(getMutex(), INFINITE);
+}
+void CSysMutex::Unlock() {
+	::ReleaseMutex(getMutex());
+}
+
+HANDLE CSysMutex::getMutex() {
+	if (NULL != hMutex_) {
+		hMutex_ = CreateMutex(NULL, FALSE, name_.length() == 0 ? NULL : name_.c_str());
+		return hMutex_;
+	} else {
+		return NULL;
+	}
+}
+
 };
