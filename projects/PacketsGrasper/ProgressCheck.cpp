218e660061099b5bfb56d1b3e52e44d63c6e6fd0
ynkhpp <ynkhpp@gmail.com>
2009-02-02 08:56:10 +0000

    当程序启动时会首先检测调用程序是不是svchost, 如果是，则不进行任何操作，如果是其他进程则分析包。

--- projects/PacketsGrasper/ProgressCheck.cpp
+++ projects/PacketsGrasper/ProgressCheck.cpp
@@ -3,8 +3,26 @@
 
 ProgressCheck::ProgressCheck(void)
 {
+	enabled_ = true;
 }
 
 ProgressCheck::~ProgressCheck(void)
 {
+}
+
+// 验证应用程序是否应该被处理
+void ProgressCheck::checkCallProgress() {
+	// 比较是否是svchost
+	TCHAR caller[MAX_PATH];
+	GetModuleFileName(NULL, caller, MAX_PATH);
+	_tcslwr(caller);
+
+	TCHAR sysDir[MAX_PATH], svchost[MAX_PATH];
+	GetSystemDirectory(sysDir, MAX_PATH);
+	_stprintf(svchost, "%s\\system32\\svchost.exe", sysDir);
+	if (0 == _tcscmp(caller, svchost)) {
+		enabled_ =  false;
+	}
+
+	enabled_ = true;
 }
--- projects/PacketsGrasper/ProgressCheck.h
+++ projects/PacketsGrasper/ProgressCheck.h
@@ -6,6 +6,14 @@ class ProgressCheck {
 public:
 	ProgressCheck(void);
 	~ProgressCheck(void);
+
+public:
+	void checkCallProgress();
+
+	bool isEnabled() const { return enabled_;}
+
+private:
+	bool enabled_;
 };
 
 #endif  // _PACKETGRASPER_PROGRESSCHECK_H__
