7804df112cc8bd40f1a7e03295d7bd89633210e4
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-02-28 09:24:45 +0000

    the unit-test project for project HistoryContent

---
+++ trunk/test/TestHistoryContent/SearchKeywordUtilTest.h
@@ -0,0 +1,20 @@
+#ifndef _TEST_HISTORY_CONTENT_SEARCHKEYWORDUTILTEST_H__
+#define _TEST_HISTORY_CONTENT_SEARCHKEYWORDUTILTEST_H__
+
+#include <searchkeywordutil.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class SearchKeywordUtilTest  : public CPPUNIT_NS::TestFixture {
+public:
+	SearchKeywordUtilTest(void);
+	~SearchKeywordUtilTest(void);
+
+	CPPUNIT_TEST_SUITE(SearchKeywordUtilTest);
+	CPPUNIT_TEST(testReadSearchWords);
+	CPPUNIT_TEST_SUITE_END();
+
+private:
+	void testReadSearchWords();
+};
+
+#endif  // _TEST_HISTORY_CONTENT_SEARCHKEYWORDUTILTEST_H__
