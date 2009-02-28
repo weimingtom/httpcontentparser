2d75e0c6bf517248599aabbec7ae957272eef680
ynkhpp <ynkhpp@gmail.com>
2009-02-28 09:24:45 +0000

    the unit-test project for project HistoryContent

---
+++ test/TestHistoryContent/SearchKeywordUtilTest.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include ".\searchkeywordutiltest.h"
+
+SearchKeywordUtilTest::SearchKeywordUtilTest(void) {
+}
+
+SearchKeywordUtilTest::~SearchKeywordUtilTest(void) {
+}
+
+
+void SearchKeywordUtilTest::testReadSearchWords() {
+}
---
+++ test/TestHistoryContent/stdafx.h
@@ -0,0 +1,32 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是常用但不常更改的项目特定的包含文件
+//
+#ifndef _TEST_HISTORY_CONTENT_STDAFX_H__
+#define _TEST_HISTORY_CONTENT_STDAFX_H__
+
+#include <iostream>
+#include <tchar.h>
+#include <winsock2.h>
+#include <windows.h>
+
+#include <cppunit\extensions\HelperMacros.h>
+#include <cppunit\ui\text\TestRunner.h>
+#include <cppunit\extensions\TestFactoryRegistry.h>
+#include <cppunit\TestResult.h>
+#include <cppunit\TestResultCollector.h>
+#include <cppunit\BriefTestProgressListener.h>
+#include <cppunit\CompilerOutputter.h> 
+
+#include <SearchkeywordUtil.h>
+
+#ifdef _DEBUG
+#	pragma comment(lib, "cppunitd.lib")
+#	pragma comment(lib, "testrunnerd.lib")
+#	pragma comment(lib, "historyContentd.lib")
+#else 
+#	pragma comment(lib, "TestRunner.lib")
+#	pragma comment(lib, "cppunit.lib")
+#	pragma comment(lib, "historyContent.lib")
+#endif
+
+#endif  // _TEST_HISTORY_CONTENT_STDAFX_H__
