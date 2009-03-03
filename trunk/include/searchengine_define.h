2a8471d848b429969e3fca73069300c49779b40f
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-03-03 02:27:58 +0000

    add a new inline function to get engine type from its host name.

--- trunk/include/searchengine_define.h
+++ trunk/include/searchengine_define.h
@@ -4,20 +4,39 @@
 #include <tchar.h>
 #include <assert.h>
 
-#define SEARCHENGINE_GOOGLE	0x00000001
+#define SEARCHENGINE_GOOGLE			0x00000001
 #define SEARCHENGINE_NAME_GOOGLE			TEXT("Google")
 #define SEARCHENGINE_NAME_GOOGLE_LEN  6
 
-#define SEARCHENGINE_YAHOO	0x00000002
+#define SEARCHENGINE_YAHOO				0x00000002
 #define SEARCHENGINE_NAME_YAHOO			TEXT("Yahoo")
 #define SEARCHENGINE_NAME_YAHOO_LEN	5
 
-#define SEARCHENGINE_BAIDU		0x00000003
+#define SEARCHENGINE_BAIDU				0x00000003
 #define SEARCHENGINE_NAME_BAIDU				TEXT("Baidu")
 #define SEARCHENGINE_NAME_BAIDU_LEN		5
 
+
+#define SEARCHENGINE_NAME_UNKNOWN		0xFFFFFFFF
 #define SEARCHENGINE_NAME_NULL				TEXT("No Search Engine")
 #define SEARCHENGINE_NAME_NULL_LEN		16
+
+#define SEARCHENGINE_HOSTNAME_GOOGLE	TEXT("google")
+#define SEARCHENGINE_HOSTNAME_YAHOO		TEXT("yahoo")
+#define SEARCHENGINE_HOSTNAME_BAIDU		TEXT("baidu")
+
+inline
+int getSearchEngineType(const char * name) {
+	if (0 == _tcscmp(SEARCHENGINE_HOSTNAME_GOOGLE, name)) {
+		return  SEARCHENGINE_GOOGLE;
+	} else if (0 == _tcscmp(SEARCHENGINE_HOSTNAME_YAHOO, name)) {
+		return SEARCHENGINE_YAHOO;
+	} else if (0 == _tcscmp(SEARCHENGINE_NAME_BAIDU, name)) {
+		return SEARCHENGINE_BAIDU;
+	} else {
+		return SEARCHENGINE_NAME_UNKNOWN;
+	}
+}
 
 inline
 int  GetSearchEngineName(const int type, TCHAR ** result) {
