fb500752cd60b557737738a7b19ebf9a9d0379b5
ynkhpp <ynkhpp@gmail.com>
2009-03-04 01:31:13 +0000

    remove some OutputDebugString

--- libproject/ServicesUtility/ServiceUtility.cpp
+++ libproject/ServicesUtility/ServiceUtility.cpp
@@ -95,11 +95,9 @@ bool checkHTTPRequest(HTTPRequestPacket * packet) {
 	// 如果不存在IP在DNS地址当中
 	// 如果DNS不可达
 	if (!isContainsIP(buffer)) {
-		// OutputDebugString("not contain ip .....");
 		if (false == checkDNS(buffer)) 
 			return false;
 	} else {
-		; // OutputDebugString("contain ip .....");
 	}
 
 	// check search rule
