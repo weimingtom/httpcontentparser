766579d809209e6a274c30a0a49e1382603aa49b
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-03-05 02:01:58 +0000

    add new interface for program control

--- trunk/projects/FilterSetting/resource.h
+++ trunk/projects/FilterSetting/resource.h
@@ -15,6 +15,7 @@
 #define IDR_IMAGECHECK                  110
 #define IDR_WEBCONTENTCHECK             111
 #define IDR_WEBCONTENTRECORD            112
+#define IDR_PROGRAMCONTROLSETTING       114
 
 // Next default values for new objects
 // 
@@ -23,6 +24,6 @@
 #define _APS_NEXT_RESOURCE_VALUE        201
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         201
-#define _APS_NEXT_SYMED_VALUE           113
+#define _APS_NEXT_SYMED_VALUE           115
 #endif
 #endif
