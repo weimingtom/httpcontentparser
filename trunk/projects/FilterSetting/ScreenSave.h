02a324bd4f04f35afd74cafe6dbb92fa05688912
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-03-09 07:33:40 +0000

    rename a class

--- trunk/projects/FilterSetting/ScreenSave.h
+++ trunk/projects/FilterSetting/ScreenSave.h
@@ -1,27 +1,27 @@
-// ScreenSave.h : CScreenSave 的声明
+// ScreenSave.h : CScreenshotRecord 的声明
 
 #pragma once
 #include "resource.h"       // 主符号
 #include "FilterSetting.h"
 #include "globalvariable.h"
 
-// CScreenSave
+// CScreenshotRecord
 
-class ATL_NO_VTABLE CScreenSave : 
+class ATL_NO_VTABLE CScreenshotRecord : 
 	public CComObjectRootEx<CComSingleThreadModel>,
-	public CComCoClass<CScreenSave, &CLSID_ScreenSave>,
+	public CComCoClass<CScreenshotRecord, &CLSID_ScreenSave>,
 	public ISupportErrorInfo,
 	public IDispatchImpl<IScreenSave, &IID_IScreenSave, &LIBID_FilterSettingLib, /*wMajor =*/ 1, /*wMinor =*/ 0>
 {
 public:
-	CScreenSave()
+	CScreenshotRecord()
 	{
 	}
 
 DECLARE_REGISTRY_RESOURCEID(IDR_SCREENSAVE)
 
 
-BEGIN_COM_MAP(CScreenSave)
+BEGIN_COM_MAP(CScreenshotRecord)
 	COM_INTERFACE_ENTRY(IScreenSave)
 	COM_INTERFACE_ENTRY(IDispatch)
 	COM_INTERFACE_ENTRY(ISupportErrorInfo)
@@ -60,4 +60,4 @@ public:
 	
 };
 
-OBJECT_ENTRY_AUTO(__uuidof(ScreenSave), CScreenSave)
+OBJECT_ENTRY_AUTO(__uuidof(ScreenSave), CScreenshotRecord)
