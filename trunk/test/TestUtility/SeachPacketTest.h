494c319612b9b34819f220e8208716b8de6aff51
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-03-16 02:26:08 +0000

    add some new unit-tests for search packet

--- trunk/test/TestUtility/SeachPacketTest.h
+++ trunk/test/TestUtility/SeachPacketTest.h
@@ -10,9 +10,15 @@ public:
 
 	CPPUNIT_TEST_SUITE(SeachPacketTest);
 	CPPUNIT_TEST(getContentTest);
+	CPPUNIT_TEST(baiduTest);
+	CPPUNIT_TEST(googleTest);
+	CPPUNIT_TEST(yahooTest);
 	CPPUNIT_TEST_SUITE_END();
 protected:
 	void getContentTest();
+	void baiduTest();
+	void googleTest();
+	void yahooTest();
 };
 
 #endif  // _UTILITY_SEACHPACKETTEST_H__
