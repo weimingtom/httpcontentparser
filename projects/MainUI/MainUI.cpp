5ce32408eac1437ab39296e52179909c62a902bc
ynkhpp <ynkhpp@gmail.com>
2009-03-17 02:26:38 +0000

    make sure the MAINUI can only run one times.

--- projects/MainUI/MainUI.cpp
+++ projects/MainUI/MainUI.cpp
@@ -7,11 +7,21 @@
 #include ".\globalvariable.h"
 #include ".\mainui.h"
 #include ".\services.h"
+#include <afxmt.h>
 #include <apputility.h>
 #include <typeconvert.h>
 #include <app_constants.h>
 #include <com\comutility.h>
 #include <AppinstallValidate.h>
+#include ".\share.h"
+
+#pragma   comment(linker,"/section:shared,rws")   
+#pragma data_seg("shared")
+HWND share_hwnd = 0;
+#pragma data_seg()
+
+#define MUTEX_NAME TEXT("2A144C85-AF84-4f88-8F7D-6C794A2800EB")
+
 
 
 IDNSSetting *g_dnssetting = NULL;
@@ -21,8 +31,6 @@ extern bool g_parentModel = false;
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
-
-
 
 // CMainUIApp
 
@@ -51,22 +59,25 @@ void initializeSetting() {
 	g_configuration.loadConfig(config_path);
 }
 
-BOOL CMainUIApp::InitInstance()
-{
+BOOL CMainUIApp::InitInstance() {
+	{
+		CMutex mutext(0, MUTEX_NAME);
+		CSingleLock(&mutext, true);
+		if (share_hwnd != NULL) {
+			ShowWindow(share_hwnd, SW_SHOW);
+			SetForegroundWindow(share_hwnd);
+			return 0;
+		}
+	}
+
 	CoInitialize(NULL);
-	
+
 	// TODO enable the following code
 	// AppInstallValidate validator(VLAIDATE_NONE);
 	// validator.repair((HMODULE)AfxGetInstanceHandle());
 
 	// ∂¡»°≈‰÷√–≈œ¢
 	initializeSetting();
-
-	HWND hwnd = GetMainUIHWND();
-	if (hwnd != NULL) {
-		::SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_SHOWWINDOW | SWP_NOMOVE);
-		return FALSE;
-	}
 
 
 	if (FALSE == Initialize()) {
