a62fd69d32e0ae987bf2ed8f6eb0d0426565eee2
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-04-06 10:01:09 +0000

    add manifesto file as resource.

--- trunk/projects/Eyecare/resource.h
+++ trunk/projects/Eyecare/resource.h
@@ -13,6 +13,7 @@
 #define IDC_EYECARE                     109
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     129
+#define IDR_RT_MANIFEST1                130
 #define IDC_PASSWORD                    1001
 #define IDC_STATIC                      -1
 
@@ -21,7 +22,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           110
