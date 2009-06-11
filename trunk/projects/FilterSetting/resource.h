d909abed7d45d3a776c6ac94fa7fb710b59a84d4
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-06-11 00:41:44 +0000

    change the icon

--- trunk/projects/FilterSetting/resource.h
+++ trunk/projects/FilterSetting/resource.h
@@ -19,12 +19,13 @@
 #define IDR_REGISTERINOF                116
 #define IDR_APPREGINFO                  117
 #define IDR_SNOWMANSETTING              118
+#define IDI_ICON1                       201
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_RESOURCE_VALUE        202
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         201
 #define _APS_NEXT_SYMED_VALUE           119
