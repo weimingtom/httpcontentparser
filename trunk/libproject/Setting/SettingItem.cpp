a764110f42ad41b4bfa5015fb60ccd7722eb2aa1
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-06-14 11:25:01 +0000

    when software is overtime, disable all functions.

--- trunk/libproject/Setting/SettingItem.cpp
+++ trunk/libproject/Setting/SettingItem.cpp
@@ -1,6 +1,11 @@
 #include "StdAfx.h"
 #include ".\settingitem.h"
+#include <softwareStatus.h>
+#include <DebugOutput.h>
 #include <assert.h>
+
+
+_INIT_FILESCOPT_OSTRSTREAM_
 
 //==========================================
 // static members
@@ -28,6 +33,12 @@ void SettingItem::defaultSetting() {
 
 
 bool SettingItem::isEnabled() const {
+	// 如果软件状态处于过期，则应该永远返回false
+	if (getAppStatus() == SNOWMAN_STATUS_OVERTIME) {
+		_DEBUG_STREAM_TRC_<<"[Setting] ["<<__FUNCTION__<< "] Trial time is over";
+		_OUTPUT_FMT_STRING_
+		return false;
+	}
 	if (current_mode_ == MODE_CHILD) {
 		return enabled_;
 	} else {
