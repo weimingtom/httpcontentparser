5567d5d178525e3f33c68a50e41c265ccfaabe57
ynkhpp <ynkhpp@gmail.com>
2009-06-14 11:25:01 +0000

    when software is overtime, disable all functions.

--- libproject/Setting/SettingItem.h
+++ libproject/Setting/SettingItem.h
@@ -16,15 +16,18 @@ public:
 protected:
 	bool enabled_;
 	void defaultSetting();
+	
 
 //====================================
 // ¿ØÖÆÊÇ·ñÏÔÊ¾
 public:
 	static int getModel()  { return current_mode_;}
 	static void setModel(const int mode);
+	static void setAppStatus(const int status) { app_status_ = status;}
+	static int getAppStatus() { return app_status_;}
 private:
 	static int current_mode_;
-
+	static int  app_status_;
 public:
 	static bool isModified() {
 		return modified_;
