7bffc8f27bc2d81bb5b1878eea6e07352f2c417d
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-06-14 14:58:23 +0000

    repair will change the uninstall status to trial status.

--- trunk/projects/Repair/Repair.cpp
+++ trunk/projects/Repair/Repair.cpp
@@ -51,11 +51,17 @@ BOOL CRepairApp::InitInstance()
 
 	CoInitialize(NULL);
 
+	// 如果用户出去OVERTIME状态，则直接退出。
+	// 无论用户处于任何状态，都有该修复
+	// 万一用户无法访问呢？
+
 	LONG app_status = SNOWMAN_STATUS_TRIAL;
 	try {
+		// 获取应用程序状态
 		ISnowmanSetting * pSetting = NULL;
 		HRESULT hr = CoCreateInstance(CLSID_SnowmanSetting, NULL, CLSCTX_LOCAL_SERVER, IID_ISnowmanSetting, (LPVOID*)&pSetting);
-		pSetting->getApplicationStatus(&app_status);
+		if (SUCCEEDED(hr)) 
+			pSetting->getApplicationStatus(&app_status);
 	} catch (...) {
 	}
 
@@ -78,8 +84,30 @@ BOOL CRepairApp::InitInstance()
 	if (NULL == _tcsstr(GetCommandLine(), "silence")) {
 		AfxMessageBox(strSucc);
 	}
+
+	// 设置状态， 如果
+	app_status = SNOWMAN_STATUS_TRIAL;
+	try {
+		// 获取应用程序状态
+		ISnowmanSetting * pSetting = NULL;
+		HRESULT hr = CoCreateInstance(CLSID_SnowmanSetting, NULL, CLSCTX_LOCAL_SERVER, IID_ISnowmanSetting, (LPVOID*)&pSetting);
+		if (SUCCEEDED(hr)) {
+			pSetting->getApplicationStatus(&app_status);
+
+			// 如果程序为卸载状态， 则设置为trial状态
+			// 这一措施用于保证用户在安装程序的时候
+			// 不需要覆盖设置， 就可以达到重新安装的目的
+			// 但是这一行为必须保证用户能够正确获取状态信息
+			// 不能是用户安装之后，从超期状态到注册状态
+
+			if (app_status == SNOWMAN_STATUS_UNINSTALL) {
+				pSetting->setApplicationStatus(SNOWMAN_STATUS_TRIAL);
+			}
+		}
+	} catch (...) {
+	}
+
 	LTRC_<<"succeeded on repair.";
-	return FALSE;
 
 
 	return TRUE;
