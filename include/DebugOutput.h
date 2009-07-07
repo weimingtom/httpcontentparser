fdf0afaa9cac8ffd623eb234e53f4a4fad298c0c
ynkhpp <ynkhpp@gmail.com>
2009-07-07 07:56:47 +0000

    add UNICODE support on OutputDebugString

--- include/DebugOutput.h
+++ include/DebugOutput.h
@@ -3,10 +3,16 @@
 
 #include <sstream>
 
+#ifdef UNICODE
+typedef  std::wstringstream     STRINGSTREAM;
+#else
+typedef  std::stringstream     STRINGSTREAM;
+#endif
+
 #ifdef  ___OUPUT_DEBUG_STRING___
-	#define _DEBUG_STREAM_OUTPUT_(TITLE, FMT) { std::stringstream __output_debug_string_stream__;	\
+	#define _DEBUG_STREAM_OUTPUT_(TITLE, FMT) { STRINGSTREAM __output_debug_string_stream__;	\
 		__output_debug_string_stream__<<TITLE<<FMT;	\
-		OutputDebugStringA(__output_debug_string_stream__.str().c_str());}\
+		OutputDebugString(__output_debug_string_stream__.str().c_str());}\
 
 	#define _DEBUG_STREAM_DBG_(FMT)	_DEBUG_STREAM_OUTPUT_("[DEG]", FMT)
 	#define _DEBUG_STREAM_TRC_(FMT)		_DEBUG_STREAM_OUTPUT_("[TRC]", FMT)
