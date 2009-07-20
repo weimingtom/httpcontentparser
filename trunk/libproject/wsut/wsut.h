546eb2fe6b853d7c0f5e6ccc46bead6d28144057
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-07-20 14:54:53 +0000

    add two function dignature, not necessary for now

--- trunk/libproject/wsut/wsut.h
+++ trunk/libproject/wsut/wsut.h
@@ -7,10 +7,12 @@
 #define WSUT_API __declspec(dllimport)
 #endif
 
-WSUT_API int __stdcall  CheckProgram(const char * status);
-WSUT_API int __stdcall  CheckStatus(const char * status);
-WSUT_API int __stdcall  CallUtility(const char * status);
-WSUT_API bool __stdcall  Installed();
-WSUT_API int __stdcall MakePrepareInstall();
-WSUT_API int __stdcall GetInstallAppPath(char *buffer, const int length);
+WSUT_API int		__stdcall		CheckProgram(const char * status);
+WSUT_API int		__stdcall		CheckStatus(const char * status);
+WSUT_API int		__stdcall		CallUtility(const char * status);
+WSUT_API bool	__stdcall		Installed();
+WSUT_API int		__stdcall		MakePrepareInstall();
+WSUT_API int		__stdcall		GetInstallAppPath(char *buffer, const int length);
+WSUT_API int		__stdcall		InstallDriver();
+WSUT_API int		__stdcall		UninstallDriver();
 #endif  // 
