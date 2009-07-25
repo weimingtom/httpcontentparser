2fc2c2be92de4fdf6ea4c51f45603d48ea043555
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-07-25 10:04:08 +0000

    add a function get SPIFilePath into

--- trunk/libproject/Apputility/AppInstallValidate.cpp
+++ trunk/libproject/Apputility/AppInstallValidate.cpp
@@ -159,10 +159,8 @@ void AppInstallValidate::repairSPI(bool removefirst) {
 
 // 安装SPI
 void AppInstallValidate::installSPI() {
-	TCHAR install_path[MAX_PATH], fullpath[MAX_PATH];
-	GetInstallPath(install_path, MAX_PATH);
-
-	_sntprintf(fullpath, MAX_PATH, TEXT("%s%s"), install_path, PACKETSGRASPER_DLL_NAME);
+	TCHAR  fullpath[MAX_PATH];
+	GetSPIInstallPath(fullpath, MAX_PATH);
 
 	// 文件不存在
 	if (_taccess(fullpath, 0) == -1) {
