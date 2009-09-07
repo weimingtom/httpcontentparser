ea023ced400fc16450e224fafde72fedb131b321
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-09-07 16:56:18 +0000

    add sqlite code, didn't make unit-test

---
+++ trunk/libproject/MassDataAccess/stdafx.h
@@ -0,0 +1,12 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是常用但不常更改的项目特定的包含文件
+//
+
+#pragma once
+#include <string>
+#include <iostream>
+#include <tchar.h>
+#include <sqlite3.h>
+
+#pragma comment(lib, "sqlite3.lib")
+
