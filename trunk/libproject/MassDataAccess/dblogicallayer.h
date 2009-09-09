6dc7232dda9764951db73907d70377cb89ebb55a
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-09-09 10:04:03 +0000

    fix bug of MassDataAccess

--- trunk/libproject/MassDataAccess/dblogicallayer.h
+++ trunk/libproject/MassDataAccess/dblogicallayer.h
@@ -22,19 +22,24 @@ int del_black_searchword(const char* word, const char* engine);
 
 // 历史记录
 int insert_website_visited(const char*  website);
-int insert_word_searched(const char*  word);
+int insert_word_searched(const char*  word, const char * engine);
 int auto_clean_website_list(const int days);
 int auto_clean_wordsearched_list(const int days);
 
 // 记录超级用户登录的时间
 int su_user_logon();
 int su_user_logoff();
+int enum_su_logon(boost::function<int (int, const char *)> f);
 
 // 
 int verify_db();             // 验证数据库
 int initialize_db();         // 初始化数据库
 int backup_db();           // 备份数据库
 int revert_db();             // 从backup中恢复数据
+
+// for test
+int insert_website_visited(const char * website, const char * timestamp);
+int insert_word_searched(const char * website, const char * engine, const char * timestamp);
 
 int set_error_msg_callback(boost::function<void (const int , const char*)> errfun);
 
