06405a90239647a8f67cb7b7eadc169edcbf3e6c
ynkhpp@gmail.com <ynkhpp@gmail.com@1a8edc88-4151-0410-b40c-1915dda2b29b>
2009-09-16 06:19:16 +0000

    add a function to replace posix_time::to_iso, which need boost lib file.

--- trunk/libproject/Utility/timeutility.h
+++ trunk/libproject/Utility/timeutility.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <time.h>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 
 namespace timeutility {
 //
@@ -17,6 +18,8 @@ TCHAR * USFormatTime(SYSTEMTIME  ft, TCHAR *buffer, INT_PTR len);
 TCHAR * USFormatTime(FILETIME ft, TCHAR * buffer, INT_PTR len);
 
 FILETIME ft_from_tm(const tm &t);
+
+std::string format_ptime_to_timestamp( boost::posix_time::ptime & pt);
 };
 
 #endif  // _UTILITY_TIMEUTILITY_H__
