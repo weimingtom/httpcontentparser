004edc4ab5fcbcadf881892a8ffb46e12daed350
ynkhpp <ynkhpp@gmail.com>
2009-09-16 15:08:03 +0000

    add Applist unit-test

--- libproject/MassDataAccess/AppList.cpp
+++ libproject/MassDataAccess/AppList.cpp
@@ -8,13 +8,21 @@ ApplicationList::ApplicationList(void) {
 ApplicationList::~ApplicationList(void) {
 }
 
+bool ApplicationList::check_path(const strutility::_tstring & fullpath) {
+    PROGRAM_INFO::iterator iter = data_.find(fullpath);
+    if (data_.end() != iter) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 int ApplicationList::add_item(const strutility::_tstring &fullpath) {
 	if (fullpath.length() == 0) {
 		return 0;
 	}
-
-	data_.insert(std::make_pair(fullpath, new FILEINFO));
+    FILEINFO info;
+	data_.insert(std::make_pair(fullpath, info));
 	return 0;
 }
 
@@ -22,33 +30,32 @@ int ApplicationList::remote_item(const strutility::_tstring &fullpath) {
 	PROGRAM_INFO::iterator iter = data_.find(fullpath);
 	if (data_.end() != iter) {
 		data_.erase(iter);
-		delete iter->second;
         return 0;
-	}
-
-	return 1;
+    } else {
+        return 1;
+    }
 }
 
 // 枚举函数
-int ApplicationList::get_first_item(strutility::_tstring *name, FILEINFO** info) {
+int ApplicationList::get_first_item(strutility::_tstring *name, FILEINFO* info) {
 	if (data_.size() == 0)
-		return 0;
+		return -1;
 	
 	// 获取数据
 	*name = data_.begin()->first;
 	*info = data_.begin()->second;
 
-	return -1;
+	return 0;
 }
-int ApplicationList::get_next_item(const strutility::_tstring & cur, strutility::_tstring *name, FILEINFO** info) {
+int ApplicationList::get_next_item(const strutility::_tstring & cur, strutility::_tstring *name, FILEINFO* info) {
 	assert(cur.length() != 0);
 
 	PROGRAM_INFO::const_iterator iter = data_.upper_bound(cur);
 	if (iter != data_.end()) {
 		*name = iter->first;
 		*info = iter->second;
-		return -1;
-	} else {
 		return 0;
+	} else {
+		return -1;
 	}
 }
---
+++ test/TestMassiveData/DataHolderTest.h
@@ -0,0 +1,8 @@
+#ifndef _DATAHOLDER_TEST_H__
+#define _DATAHOLDER_TEST_H__
+
+int DataHolderTest_ApplicationList();
+int DataHolderTest_WebsitesList();
+int DataHolderTest_WordsList();
+
+#endif // _DATAHOLDER_TEST_H__
