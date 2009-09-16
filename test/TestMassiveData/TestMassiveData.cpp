4e7dc129c4c50fdbe08442423ee9ab4d21833173
ynkhpp <ynkhpp@gmail.com>
2009-09-16 15:08:43 +0000

    adjust the project folder structure

--- test/TestMassiveData/TestMassiveData.cpp
+++ test/TestMassiveData/TestMassiveData.cpp
@@ -3,12 +3,16 @@
 
 #include "stdafx.h"
 #include "LogicalLayerTest.h"
+#include "dataholderTest.h"
 #include <boost\test\included\unit_test.hpp>
 using namespace boost::unit_test;
 
 
 
 boost::unit_test::test_suite* init_unit_test_suite( int argc, char* argv[] ) {
+
+    //========================================
+    // LogicalLayerTest.h
     framework::master_test_suite().add( BOOST_TEST_CASE(&init_db) );
     framework::master_test_suite().add( BOOST_TEST_CASE(&test_auto_searchword_clean) );
     framework::master_test_suite().add( BOOST_TEST_CASE(&test_auto_website_clean) );
@@ -16,5 +20,12 @@ boost::unit_test::test_suite* init_unit_test_suite( int argc, char* argv[] ) {
     framework::master_test_suite().add( BOOST_TEST_CASE(&test_blasklisted_dns) );
     framework::master_test_suite().add( BOOST_TEST_CASE(&test_blasklisted_word) );
     framework::master_test_suite().add( BOOST_TEST_CASE(&test_sulogon) );
+
+    //========================================
+    // LogicalLayerTest.h
+    framework::master_test_suite().add( BOOST_TEST_CASE(&DataHolderTest_ApplicationList) );
+    framework::master_test_suite().add( BOOST_TEST_CASE(&DataHolderTest_WebsitesList) );
+    framework::master_test_suite().add( BOOST_TEST_CASE(&DataHolderTest_WordsList) );
+
     return 0;
 }
