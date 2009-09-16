efe55cbbc94ef845a8f3f81338a3a6362f53e564
ynkhpp <ynkhpp@gmail.com>
2009-09-16 06:17:09 +0000

    forget to add file

---
+++ test/TestMassiveData/LogicalLayerTest.h
@@ -0,0 +1,12 @@
+#ifndef _LOGICALLAYERTEST_H__
+#define _LOGICALLAYERTEST_H__
+
+int init_db();
+int test_whitelisted_dns();
+int test_blasklisted_dns();
+int test_blasklisted_word();
+int test_auto_searchword_clean();
+int test_auto_website_clean();
+int test_sulogon();
+
+#endif // _LOGICALLAYERTEST_H__
